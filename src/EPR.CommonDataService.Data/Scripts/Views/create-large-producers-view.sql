a654294f041f3864a5273e9adacd38962d5ac193
Build Service <buildservice@dev.azure.com>
2024-03-26 06:09:03 +0000

    Refreshed from ADO

--- src/EPR.CommonDataService.Data/Scripts/Views/create-large-producers-view.sql
+++ src/EPR.CommonDataService.Data/Scripts/Views/create-large-producers-view.sql
@@ -6,31 +6,30 @@ GO
 DROP VIEW IF EXISTS [apps].[v_Large_Producers]
 GO
 
--- Environmental regulator	Reporting year	Org name	Trading name	Companies House number 	Org ID	Subsidiary ID	Address line 1 	Address line 2 	Address line 3	Address line 4 	Town	County	Postcode	Compliance scheme	Compliance scheme regulator
-CREATE VIEW [apps].[v_Large_Producers] AS
-SELECT *
+CREATE VIEW [apps].[v_Large_Producers]
+AS SELECT DISTINCT *
 FROM (
     SELECT DISTINCT
     Pom.organisation_id AS 'RPD_Organisation_ID'
-    , Pom.submission_period
+    , '' AS 'submission_period'
     , cs.Name AS 'Compliance_scheme'
     -- COALESCE ALL COMPANY DETAILS FIELDS
-    , COALESCE( CompanyDetails.companies_house_number, producer.CompaniesHouseNumber) AS 'Companies_House_number'
-    , COALESCE( CompanyDetails.subsidiary_id, Pom.subsidiary_id) AS 'Subsidiary_ID'
-    , COALESCE( CompanyDetails.organisation_name, producer.Name ) AS 'Organisation_name'
-    , COALESCE( CompanyDetails.Trading_Name, producer.TradingName ) AS 'Trading_name'
-    , COALESCE( CompanyDetails.registered_addr_line1, (
-            TRIM(producer.BuildingNumber) + ' ' +
-            TRIM(producer.BuildingName)
-        )
+    , COALESCE( producer.CompaniesHouseNumber, CompanyDetails.companies_house_number) AS 'Companies_House_number'
+    , COALESCE(NULLIF(CompanyDetails.subsidiary_id, ''), NULLIF(Pom.subsidiary_id, ''), '') AS 'Subsidiary_ID'
+    , COALESCE( producer.Name, CompanyDetails.organisation_name ) AS 'Organisation_name'
+    , COALESCE( producer.TradingName, CompanyDetails.Trading_Name ) AS 'Trading_name'
+    , COALESCE( (
+            TRIM(producer.BuildingName) + ' ' +
+            TRIM(producer.BuildingNumber) 
+        ), CompanyDetails.registered_addr_line1
     ) AS 'Address_line_1'
-    , COALESCE( CompanyDetails.registered_addr_line2, producer.Street ) AS 'Address_line_2'
+    , COALESCE(producer.Street, CompanyDetails.registered_addr_line2) AS 'Address_line_2'
     , 'Address_line_3' = ''
     , 'Address_line_4' = ''
-    , COALESCE( CompanyDetails.registered_city, producer.Town ) AS 'Town'
-    , COALESCE( CompanyDetails.registered_addr_county, producer.County) AS 'County'
-    , COALESCE( CompanyDetails.registered_addr_country, producer.Country ) AS 'Country'
-    , COALESCE( CompanyDetails.registered_addr_postcode, producer.Postcode ) AS 'Postcode'
+    , COALESCE( producer.Town, CompanyDetails.registered_city) AS 'Town'
+    , producer.County AS 'County'
+    , COALESCE( producer.Country, CompanyDetails.registered_addr_country) AS 'Country'
+    , COALESCE( producer.Postcode, CompanyDetails.registered_addr_postcode) AS 'Postcode'
     , producernation.Name AS ProducerNation
     , producernation.Id AS ProducerNationId
     , csnation.Name AS ComplianceSchemeNation
@@ -52,7 +51,7 @@ FROM (
     --, 'POM_START' AS StartPoint
     --, e_status.EnrolmentStatuses_EnrolmentStatus as e_status
     FROM [rpd].[Pom]
-
+ 
     LEFT JOIN (
         SELECT CompanyDetails.*
         FROM [rpd].[CompanyDetails]
@@ -60,7 +59,7 @@ FROM (
             ON CompanyDetails.organisation_id = rl.organisation_id
     ) CompanyDetails
         ON Pom.Organisation_id = CompanyDetails.organisation_id
-    LEFT JOIN rpd.cosmos_file_metadata meta
+    INNER JOIN apps.get_latest_pom_file_submitted meta
         ON Pom.FileName = meta.FileName
     LEFT JOIN rpd.ComplianceSchemes cs
         ON meta.ComplianceSchemeId = cs.ExternalId
@@ -78,32 +77,33 @@ FROM (
        AND (cs.IsDeleted = 0 OR cs.IsDeleted IS NULL)  ---> If only company-details file is submitted cs.IsDeleted would be NULL
        AND (producer.isdeleted = 0 OR producer.isdeleted IS NULL)
        AND e_status.EnrolmentStatuses_EnrolmentStatus <> 'Rejected'
+       AND (producer.IsComplianceScheme = 0 OR producer.IsComplianceScheme IS NULL)
 ) pom_start
-
-UNION 
-
-SELECT * FROM (
+ 
+UNION
+ 
+SELECT DISTINCT * FROM (
     SELECT DISTINCT
     CompanyDetails.organisation_id AS 'RPD_Organisation_ID'
-    , meta.submissionperiod
+    , '' AS 'submission_period'
     , cs.Name AS 'Compliance_scheme'
     -- COALESCE ALL COMPANY DETAILS FIELDS
-    , COALESCE( CompanyDetails.companies_house_number, producer.CompaniesHouseNumber) AS 'Companies_House_number'
+    , COALESCE( producer.CompaniesHouseNumber, CompanyDetails.companies_house_number) AS 'Companies_House_number'
     , COALESCE( CompanyDetails.subsidiary_id, '') AS 'Subsidiary_ID'
-    , COALESCE( CompanyDetails.organisation_name, producer.Name ) AS 'Organisation_name'
-    , COALESCE( CompanyDetails.Trading_Name, producer.TradingName ) AS 'Trading_name'
-    , COALESCE( CompanyDetails.registered_addr_line1, (
-            TRIM(producer.BuildingNumber) + ' ' +
-            TRIM(producer.BuildingName)
-        )
-    )  AS 'Address_line_1'
-    , COALESCE( CompanyDetails.registered_addr_line2, producer.Street ) AS 'Address_line_2'
+    , COALESCE( producer.Name, CompanyDetails.organisation_name ) AS 'Organisation_name'
+    , COALESCE( producer.TradingName, CompanyDetails.Trading_Name ) AS 'Trading_name'
+    , COALESCE( (
+            TRIM(producer.BuildingName) + ' ' +
+            TRIM(producer.BuildingNumber) 
+        ), CompanyDetails.registered_addr_line1
+    ) AS 'Address_line_1'
+    , COALESCE(producer.Street, CompanyDetails.registered_addr_line2) AS 'Address_line_2'
     , 'Address_line_3' = ''
     , 'Address_line_4' = ''
-    , COALESCE( CompanyDetails.registered_city, producer.Town ) AS 'Town'
-    , COALESCE( CompanyDetails.registered_addr_county, producer.County) AS 'County'
-    , COALESCE( CompanyDetails.registered_addr_country, producer.Country ) AS 'Country'
-    , COALESCE( CompanyDetails.registered_addr_postcode, producer.Postcode ) AS 'Postcode'
+    , COALESCE( producer.Town, CompanyDetails.registered_city) AS 'Town'
+    , producer.County AS 'County'
+    , COALESCE( producer.Country, CompanyDetails.registered_addr_country) AS 'Country'
+    , COALESCE( producer.Postcode, CompanyDetails.registered_addr_postcode) AS 'Postcode'
     , producernation.Name AS ProducerNation
     , producernation.Id AS ProducerNationId
     , csnation.Name AS ComplianceSchemeNation
@@ -125,8 +125,8 @@ SELECT * FROM (
     --, 'REG_START' AS StartPoint
     -- , e_status.EnrolmentStatuses_EnrolmentStatus as e_status
     FROM [rpd].[CompanyDetails]
-
-    LEFT JOIN v_cosmos_file_metadata meta
+ 
+    INNER JOIN apps.get_latest_org_file_submitted meta
         ON [CompanyDetails].FileName = meta.FileName
     LEFT JOIN rpd.ComplianceSchemes cs
         ON meta.ComplianceSchemeId = cs.ExternalId
@@ -142,12 +142,9 @@ SELECT * FROM (
           FROM t_rpd_data_SECURITY_FIX
           GROUP BY FromOrganisation_ReferenceNumber, EnrolmentStatuses_EnrolmentStatus) e_status
         ON e_status.FromOrganisation_ReferenceNumber = CompanyDetails.organisation_id
-
+ 
      WHERE (cs.IsDeleted = 0 OR cs.IsDeleted IS NULL)  ---> If only company-details file is submitted cs.IsDeleted would be NULL
           AND (producer.isdeleted = 0 OR producer.isdeleted IS NULL)
           AND e_status.EnrolmentStatuses_EnrolmentStatus <> 'Rejected'
 ) reg_start;
-
--- The ORDER BY clause is not valid in views
--- ORDER BY 'Companies House Number', subsidiary_id 
 GO
