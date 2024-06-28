61264d395e5a0f50615e861903a0fa8030bdb565
Simon Crowder (DEV3) <155532504+scdefra@users.noreply.github.com>
2024-06-28 09:46:20 +0100

    Updated the view (#19)
    * Updated the view
    
    * Added missing drop code

--- src/EPR.CommonDataService.Data/Scripts/Views/create-large-producers-view.sql
+++ src/EPR.CommonDataService.Data/Scripts/Views/create-large-producers-view.sql
@@ -6,16 +6,19 @@ GO
 DROP VIEW IF EXISTS [apps].[v_Large_Producers]
 GO
 
-CREATE VIEW [apps].[v_Large_Producers]
-AS 
-	select 
+CREATE VIEW [apps].[v_Large_Producers] AS 
+
+select 
+	--Reporting_year, Organisation_name, Trading_name, Companies_House_number, RPD_Organisation_ID, Subsidiary_ID, Address_line_1, Address_line_2, Town, County, Country, Postcode
+		
 		RPD_Organisation_ID,		submission_period,		Compliance_scheme,		Companies_House_number,		Subsidiary_ID,		Organisation_name,
 		Trading_name,		Address_line_1,		Address_line_2,		Address_line_3,		Address_line_4,		Town,		County,		Country,
 		Postcode,		ProducerNation,		ProducerNationId,		ComplianceSchemeNation,		ComplianceSchemeNationId,		ProducerId,
 		Environmental_regulator,		Compliance_scheme_regulator,		Reporting_year
+		
 	from
 	(
-		select *, Row_number() over(Partition by RPD_Organisation_ID, Subsidiary_ID order by SubmittedDateTime desc) as rn
+		select *, Row_number() over(Partition by RPD_Organisation_ID, Subsidiary_ID, Reporting_year order by SubmittedDateTime desc) as rn
 		from
 		(	
 	
@@ -25,11 +28,10 @@ AS
 									p.organisation_id AS 'RPD_Organisation_ID'
 									, '' AS 'submission_period'
 									, cs.Name AS 'Compliance_scheme'
-									, COALESCE( v.companies_house_number, prr.CompaniesHouseNumber) AS 'Companies_House_number'
+									, case when v.organisation_id is not null then v.companies_house_number else prr.CompaniesHouseNumber end as 'Companies_House_number'
 									, COALESCE(v.subsidiary_id, p.subsidiary_id, '') AS 'Subsidiary_ID'
-									, COALESCE( v.organisation_name, prr.Name  ) AS 'Organisation_name'
-									, COALESCE( v.Trading_Name, prr.TradingName  ) AS 'Trading_name'
-
+									, case when v.organisation_id is not null then v.organisation_name else prr.Name end as 'Organisation_name'
+									, case when v.organisation_id is not null then v.Trading_Name else prr.TradingName end as 'Trading_name'
 									, case 
 										when v.organisation_id is not null 
 											then ISNULL(v.registered_addr_line1,'') 
@@ -79,14 +81,15 @@ AS
 										WHEN 3 THEN 'Scottish Environment Protection Agency'
 										WHEN 4 THEN 'Natural Resources Wales'
 									  END) As 'Compliance_scheme_regulator'
-									, 2023 as Reporting_year
-									,m.created SubmittedDateTime
+									,'20'+reverse(substring(reverse(trim(meta.SubmissionPeriod)),1,2)) as 'Reporting_year'
+									,meta.created SubmittedDateTime
 								FROM [rpd].[Pom] p
-								left join [dbo].[v_registration_latest] v
+								left join [dbo].[v_cosmos_file_metadata] meta
+									on meta.FileName = p.FileName
+								left join [dbo].[v_registration_latest_by_Year] v
 									ON p.Organisation_id = v.organisation_id
 										and isnull(p.subsidiary_id,'') = isnull(v.subsidiary_id,'')
-								left JOIN apps.get_latest_pom_file_submitted meta
-									ON p.FileName = meta.FileName
+										and v.Reporting_year = '20'+reverse(substring(reverse(trim(meta.SubmissionPeriod)),1,2))
 								LEFT JOIN rpd.ComplianceSchemes cs
 									ON meta.ComplianceSchemeId = cs.ExternalId
 								left JOIN rpd.Organisations prr
@@ -95,8 +98,6 @@ AS
 									ON prr.NationId = producernation.Id
 								LEFT JOIN rpd.Nations csnation
 									ON cs.NationId = csnation.Id
-								left join [dbo].[v_cosmos_file_metadata] m
-									on m.FileName = p.FileName
 								left JOIN (SELECT FromOrganisation_ReferenceNumber, EnrolmentStatuses_EnrolmentStatus
 												FROM t_rpd_data_SECURITY_FIX
 												GROUP BY FromOrganisation_ReferenceNumber, EnrolmentStatuses_EnrolmentStatus) e_status
@@ -114,10 +115,10 @@ AS
 								cd.organisation_id AS 'RPD_Organisation_ID'
 								, '' AS 'submission_period'
 								, cs.Name AS 'Compliance_scheme'
-								, COALESCE( cd.companies_house_number, pr.CompaniesHouseNumber) AS 'Companies_House_number'
+								, case when cd.organisation_id is not null then cd.companies_house_number else pr.CompaniesHouseNumber end as 'Companies_House_number'
 								, COALESCE( cd.subsidiary_id, '') AS 'Subsidiary_ID'
-								, COALESCE( cd.organisation_name , pr.Name ) AS 'Organisation_name'
-								, COALESCE( cd.Trading_Name , pr.TradingName ) AS 'Trading_name'
+								, case when cd.organisation_id is not null then cd.organisation_name else pr.Name end as 'Organisation_name'
+								, case when cd.organisation_id is not null then cd.Trading_Name else pr.TradingName end as 'Trading_name'
 								, case 
 									when cd.organisation_id is not null 
 										then ISNULL(cd.registered_addr_line1,'') 
@@ -167,34 +168,33 @@ AS
 									WHEN 3 THEN 'Scottish Environment Protection Agency'
 									WHEN 4 THEN 'Natural Resources Wales'
 									END) As 'Compliance_scheme_regulator'
-								, 2023 as Reporting_year
-								,m.created SubmittedDateTime
+								,'20'+reverse(substring(reverse(trim(meta.SubmissionPeriod)),1,2)) as 'Reporting_year'
+								, meta.created SubmittedDateTime
 							FROM [rpd].[CompanyDetails] cd
- 							left JOIN apps.get_latest_org_file_submitted meta
-								ON cd.FileName = meta.FileName
+ 							left join [dbo].[v_cosmos_file_metadata] meta
+								on meta.FileName = cd.FileName
 							LEFT JOIN rpd.ComplianceSchemes cs
 								ON meta.ComplianceSchemeId = cs.ExternalId
 							left JOIN rpd.Organisations pr
 								ON cd.organisation_id = pr.ReferenceNumber
-							LEFT JOIN rpd.Nations producernation  ---> 'LEFT JOIN instead of JOIN to take data even if enrolment data doesn't exist' (should use it)
+							LEFT JOIN rpd.Nations producernation 
 								ON pr.NationId = producernation.Id
 							LEFT JOIN rpd.Nations csnation
 								ON cs.NationId = csnation.Id
-							left JOIN [dbo].[v_registration_latest] rl
-								ON cd.organisation_id = rl.organisation_id-- AND rl.created = meta.created
+							left JOIN [dbo].[v_registration_latest_by_Year] rl
+								ON cd.organisation_id = rl.organisation_id
 								and isnull(cd.subsidiary_id,'') = isnull(rl.subsidiary_id,'')
+								and rl.Reporting_year = '20'+reverse(substring(reverse(trim(meta.SubmissionPeriod)),1,2))
 							left JOIN (SELECT FromOrganisation_ReferenceNumber, EnrolmentStatuses_EnrolmentStatus
 									FROM t_rpd_data_SECURITY_FIX
 									GROUP BY FromOrganisation_ReferenceNumber, EnrolmentStatuses_EnrolmentStatus) e_status
 								ON e_status.FromOrganisation_ReferenceNumber = cd.organisation_id
- 							left join [dbo].[v_cosmos_file_metadata] m
-								on m.FileName = cd.FileName
-								WHERE (cs.IsDeleted = 0 OR cs.IsDeleted IS NULL)  ---> If only company-details file is submitted cs.IsDeleted would be NULL
+								WHERE (cs.IsDeleted = 0 OR cs.IsDeleted IS NULL)
 									AND (pr.isdeleted = 0 OR pr.isdeleted IS NULL)
 									AND e_status.EnrolmentStatuses_EnrolmentStatus <> 'Rejected'
 									AND (pr.IsComplianceScheme = 0 OR pr.IsComplianceScheme IS NULL)
 						) reg_start
 		) A
 	) B
-	where B.rn = 1;
+	where B.rn = 1; --and B.Reporting_year in (2023,2024);
 GO
