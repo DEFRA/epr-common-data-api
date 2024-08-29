58602d1c903896b00ada07d8be90a861d8fad166
taseakpoexternal <tase.akpo.external@eviden.com>
2024-08-29 12:05:25 +0100

    Refactoring Existing Common Data API Endpoints (#50)
    * get approved submissions endpoint created
    
    * tests for approved submissions
    
    * initial commit for GetAggregatedPomData
    
    * add unit tests
    
    * add get-aggregated-pom-data sproc
    
    * fix typo
    
    * fix another typo
    
    * fix scripts to resolve finding the correct data
    
    * 426955 - Refactoring Existing Common Data API Endpoints
    
    ---------
    
    Co-authored-by: Steven Moschidis <steven.moschidis.external@eviden.com>

--- src/EPR.CommonDataService.Data/Entities/ApprovedSubmissionEntity.cs
+++ src/EPR.CommonDataService.Data/Entities/ApprovedSubmissionEntity.cs
@@ -3,5 +3,9 @@
     public class ApprovedSubmissionEntity
     {
         public Guid SubmissionId { get; set; }
+        public string SubmissionPeriod { get; set; }
+        public string PackagingMaterial { get; set; }
+        public double PackagingMaterialWeight { get; set; }
+        public int OrganisationId { get; set; }
     }
 }
---
+++ src/EPR.CommonDataService.Data/Scripts/Stored Procedures/get-approved-submissions-with-aggregated-pom-data.sql
@@ -0,0 +1,54 @@
+IF EXISTS (SELECT 1 FROM sys.procedures WHERE object_id = OBJECT_ID(N'[rpd].[sp_GetApprovedSubmissionsWithAggregatedPomData]'))
+BEGIN
+    DROP PROCEDURE [rpd].[sp_GetApprovedSubmissionsWithAggregatedPomData]
+END
+GO
+
+CREATE PROCEDURE [rpd].[sp_GetApprovedSubmissionsWithAggregatedPomData]
+    @ApprovedAfter DATETIME2
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    WITH ApprovedSubmissions AS
+    (
+        SELECT SubmissionId
+        FROM [rpd].[SubmissionEvents]
+        WHERE TRY_CAST([Created] AS datetime2) > @ApprovedAfter
+          AND Decision = 'Accepted'
+    ),
+    FileIds AS
+    (
+        SELECT s.SubmissionId, se.FileId
+        FROM ApprovedSubmissions s
+        CROSS APPLY
+        (
+            SELECT TOP 1 FileId
+            FROM [rpd].[SubmissionEvents] se
+            WHERE se.SubmissionId = s.SubmissionId
+              AND se.FileId IS NOT NULL
+            ORDER BY se.Created DESC
+        ) se
+    ),
+    FileNames AS
+    (
+        SELECT f.SubmissionId, fm.[FileName]
+        FROM FileIds f
+        JOIN [rpd].[cosmos_file_metadata] fm ON f.FileId = fm.FileId
+    )
+    SELECT 
+        f.SubmissionId,
+        p.submission_period,
+        p.packaging_material,
+        SUM(p.packaging_material_weight) AS packaging_material_weight,
+        p.organisation_id
+    FROM FileNames f
+    JOIN [rpd].[Pom] p ON p.[FileName] = f.[FileName]
+    WHERE LEFT(p.submission_period, 4) = CAST(YEAR(@ApprovedDate) AS VARCHAR(4))
+    GROUP BY 
+        f.SubmissionId,
+        p.submission_period,
+        p.packaging_material,
+        p.organisation_id;
+END
+GO
