e9d17643577a3be2f8ff5eb85fe53b7ac99313d7
Hardik Desai <hardikkumar.desai.external@eviden.com>
2024-10-29 11:26:04 +0000

    removed unused code

--- src/EPR.CommonDataService.Core/Models/Response/GetOnlineMarketplaceFlagResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EPR.CommonDataService.Core.Models.Response;
-
-public class GetOnlineMarketplaceFlagResponse
-{
-    public Guid OrganisationId { get; set; }
-
-    public bool IsOnlineMarketPlace { get; set; }
-}
