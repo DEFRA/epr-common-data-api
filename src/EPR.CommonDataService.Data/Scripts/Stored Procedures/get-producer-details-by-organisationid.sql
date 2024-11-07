a203f1d0668cc00198b66f2e94ca0c90a64bfc08
Salman Qayyum (Eviden) <Salman.Qayyum@Defra.onmicrosoft.com>
2024-11-07 09:53:23 +0000

    Added sp for produced detail 470112

---
+++ src/EPR.CommonDataService.Data/Scripts/Stored Procedures/get-producer-details-by-organisationid.sql
@@ -0,0 +1,54 @@
+﻿-- Dropping stored procedure if it exists
+IF EXISTS (SELECT 1 FROM sys.procedures WHERE object_id = OBJECT_ID(N'[apps].[sp_GetProducerDetailsByOrganisationId]'))
+DROP PROCEDURE [apps].[sp_GetProducerDetailsByOrganisationId];
+GO
+
+CREATE PROCEDURE apps.sp_GetProducerDetailsByOrganisationId
+    @organisationId INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    WITH SubsidiaryCount AS (
+        SELECT 
+            organisation_id, 
+            COUNT(*) AS NumberOfSubsidiaries
+        FROM 
+            rpd.companyDetails
+        WHERE 
+            organisation_id = @organisationId 
+            AND subsidiary_id IS NULL
+        GROUP BY 
+            organisation_id
+    )
+
+    SELECT 
+        COUNT(cd.packaging_activity_om) AS NumberOfSubsidiariesBeingOnlineMarketPlace,
+        cd.packaging_activity_om,
+        cd.organisation_id,
+        pom.organisation_size,
+        '' AS ApplicationReferenceNumber,
+        sc.NumberOfSubsidiaries
+    FROM 
+        rpd.companyDetails cd
+        INNER JOIN rpd.Organisations org 
+            ON org.referenceNumber = cd.organisation_id
+        LEFT JOIN dbo.t_POM pom 
+            ON pom.organisation_id = org.referenceNumber
+        LEFT JOIN dbo.t_POM_Submissions tps 
+            ON tps.organisation_id = cd.organisation_id
+        INNER JOIN rpd.Submissions sub 
+            ON sub.organisationid = org.externalid
+        LEFT JOIN SubsidiaryCount sc 
+            ON sc.organisation_id = cd.organisation_id
+    WHERE 
+        cd.packaging_activity_om IN ('Primary', 'Secondary')
+        AND cd.organisation_id = @organisationId
+    GROUP BY 
+        cd.packaging_activity_om, 
+        cd.organisation_id,
+        pom.organisation_size,
+        sc.NumberOfSubsidiaries;
+END;
+
+GO
