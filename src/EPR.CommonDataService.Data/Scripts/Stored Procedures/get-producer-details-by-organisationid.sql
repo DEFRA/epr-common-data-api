00db31b0dbd987ce7ce69308fd9a283fef59145a
Salman Qayyum (Eviden) <Salman.Qayyum@Defra.onmicrosoft.com>
2024-11-11 22:29:33 +0000

    added code review changes

--- src/EPR.CommonDataService.Data/Scripts/Stored Procedures/get-producer-details-by-organisationid.sql
+++ src/EPR.CommonDataService.Data/Scripts/Stored Procedures/get-producer-details-by-organisationid.sql
@@ -10,45 +10,48 @@ BEGIN
     SET NOCOUNT ON;
 
     WITH SubsidiaryCount AS (
-        SELECT 
-            organisation_id, 
-            COUNT(*) AS NumberOfSubsidiaries
-        FROM 
-            rpd.companyDetails
-        WHERE 
-            organisation_id = @organisationId 
-            AND subsidiary_id IS NULL
-        GROUP BY 
-            organisation_id
-    )
-
     SELECT 
-        COUNT(cd.packaging_activity_om) AS NumberOfSubsidiariesBeingOnlineMarketPlace,
-        cd.packaging_activity_om,
-        cd.organisation_id,
-        pom.organisation_size,
-        '' AS ApplicationReferenceNumber,
-        sc.NumberOfSubsidiaries
+        organisation_id, 
+        COUNT(*) AS NumberOfSubsidiaries
     FROM 
-        rpd.companyDetails cd
-        INNER JOIN rpd.Organisations org 
-            ON org.referenceNumber = cd.organisation_id
-        LEFT JOIN dbo.t_POM pom 
-            ON pom.organisation_id = org.referenceNumber
-        LEFT JOIN dbo.t_POM_Submissions tps 
-            ON tps.organisation_id = cd.organisation_id
-        INNER JOIN rpd.Submissions sub 
-            ON sub.organisationid = org.externalid
-        LEFT JOIN SubsidiaryCount sc 
-            ON sc.organisation_id = cd.organisation_id
+        rpd.companyDetails
     WHERE 
-        cd.packaging_activity_om IN ('Primary', 'Secondary')
-        AND cd.organisation_id = @organisationId
+        organisation_id = @organisationId 
+        AND subsidiary_id IS NULL
     GROUP BY 
-        cd.packaging_activity_om, 
-        cd.organisation_id,
-        pom.organisation_size,
-        sc.NumberOfSubsidiaries;
+        organisation_id
+)
+
+SELECT 
+    COUNT(cd.packaging_activity_om) AS NumberOfSubsidiariesBeingOnlineMarketPlace,
+    cd.organisation_id,
+    CASE 
+        WHEN cd.packaging_activity_om IN ('Primary', 'Secondary') THEN CAST(1 AS BIT)
+        ELSE CAST(0 AS BIT)
+    END AS IsOnlineMarketplace,
+    pom.organisation_size AS 'ProducerSize',
+    '' AS ApplicationReferenceNumber,
+    sc.NumberOfSubsidiaries
+FROM 
+    rpd.companyDetails cd
+    INNER JOIN rpd.Organisations org 
+        ON org.referenceNumber = cd.organisation_id
+    LEFT JOIN dbo.t_POM pom 
+        ON pom.organisation_id = org.referenceNumber
+    LEFT JOIN dbo.t_POM_Submissions tps 
+        ON tps.organisation_id = cd.organisation_id
+    INNER JOIN rpd.Submissions sub 
+        ON sub.organisationid = org.externalid
+    LEFT JOIN SubsidiaryCount sc 
+        ON sc.organisation_id = cd.organisation_id
+WHERE 
+    cd.organisation_id = @organisationId
+GROUP BY 
+    cd.packaging_activity_om, 
+    cd.organisation_id,
+    pom.organisation_size,
+    sc.NumberOfSubsidiaries;
+    
 END;
 
 GO
