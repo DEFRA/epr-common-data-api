3df3a2ecdac69b3cf6f7336565c925b737c11e27
Harish Paladugu <harish.paladugu.external@eviden.com>
2024-11-11 08:43:51 +0000

    Added logging to Submissions controller and service (#62)
    * Added logging to Submissions controller and service
    
    * remove #nullable directive
    
    * Added default config value

--- src/EPR.CommonDataService.Api/Controllers/SubmissionsController.cs
+++ src/EPR.CommonDataService.Api/Controllers/SubmissionsController.cs
@@ -1,19 +1,20 @@
+using Azure.Core;
 using EPR.CommonDataService.Api.Configuration;
 using EPR.CommonDataService.Core.Models.Requests;
 using EPR.CommonDataService.Core.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using System.Globalization;
 
 namespace EPR.CommonDataService.Api.Controllers;
 
 [ApiController]
 [Route("api/submissions")]
-public class SubmissionsController(
-    ISubmissionsService submissionsService,
-    IOptions<ApiConfig> baseApiConfigOptions)
-    : ApiControllerBase(baseApiConfigOptions)
+public class SubmissionsController(ISubmissionsService submissionsService, IOptions<ApiConfig> baseApiConfigOptions, ILogger<SubmissionsController> logger, IConfiguration config) : ApiControllerBase(baseApiConfigOptions)
 {
+    private readonly string? logPrefix = string.IsNullOrEmpty(config["LogPrefix"]) ? "[EPR.CommonDataService]" : config["LogPrefix"];
+
     private const string Periods = "P1,P4"; //will be added to config in future story
 
     [HttpPost("pom/summary")]
@@ -23,8 +24,11 @@ public class SubmissionsController(
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetPomSubmissionsSummaries(SubmissionsSummariesRequest<RegulatorPomDecision> request)
     {
+        logger.LogInformation("{Logprefix}: SubmissionsController: Api Route 'pom/summary'", logPrefix);
+        logger.LogInformation("{Logprefix}: SubmissionsController - GetPomSubmissionsSummaries: Get Pom Submissions for given Regulator {PomSubmissions}", logPrefix, JsonConvert.SerializeObject(request));
         var result = await submissionsService.GetSubmissionPomSummaries(request);
 
+        logger.LogInformation("{Logprefix}: SubmissionsController - GetPomSubmissionsSummaries: Pom Submissions returned {SubmissionPomSummaries}", logPrefix, result);
         return Ok(result);
     }
 
@@ -35,8 +39,11 @@ public class SubmissionsController(
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetRegistrationsSubmissionsSummaries(SubmissionsSummariesRequest<RegulatorRegistrationDecision> request)
     {
+        logger.LogInformation("{Logprefix}: SubmissionsController: Api Route 'registrations/summary'", logPrefix);
+        logger.LogInformation("{Logprefix}: SubmissionsController - GetRegistrationsSubmissionsSummaries: Get Registration Submissions for given Regulator {RegulatorSubmissions}", logPrefix, JsonConvert.SerializeObject(request));
         var result = await submissionsService.GetSubmissionRegistrationSummaries(request);
 
+        logger.LogInformation("{Logprefix}: SubmissionsController - GetRegistrationsSubmissionsSummaries: Registration Submissions returned {SubmissionRegistrationSummaries}", logPrefix, result);
         return Ok(result);
     }
 
@@ -54,10 +61,12 @@ public class SubmissionsController(
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetApprovedSubmissionsWithAggregatedPomData(string approvedAfterDateString)
     {
+        logger.LogInformation("{Logprefix}: SubmissionsController: Api Route 'v1/pom/approved/{ApprovedAfterDateString}'", logPrefix, approvedAfterDateString);
+        logger.LogInformation("{Logprefix}: SubmissionsController - GetApprovedSubmissionsWithAggregatedPomData: Get submissions approved after {ApprovedAfterDateString}", logPrefix, approvedAfterDateString);
         if (!DateTime.TryParse(approvedAfterDateString, CultureInfo.InvariantCulture.DateTimeFormat, DateTimeStyles.AssumeUniversal, out var approvedAfter))
         {
+            logger.LogError("{Logprefix}: SubmissionsController - GetApprovedSubmissionsWithAggregatedPomData: Invalid datetime provided; please make sure it's a valid UTC datetime", logPrefix);
             ModelState.AddModelError(nameof(approvedAfterDateString), "Invalid datetime provided; please make sure it's a valid UTC datetime");
-
             return BadRequest(ModelState);
         }
 
@@ -67,18 +76,22 @@ public class SubmissionsController(
 
             if (!approvedSubmissions.Any())
             {
+                logger.LogError("{Logprefix}: SubmissionsController - GetApprovedSubmissionsWithAggregatedPomData: The datetime provided did not return any submissions", logPrefix);
                 ModelState.AddModelError(nameof(approvedAfterDateString), "The datetime provided did not return any submissions");
                 return NotFound(ModelState);
             }
 
+            logger.LogInformation("{Logprefix}: SubmissionsController - GetApprovedSubmissionsWithAggregatedPomData: Approved Submissions returned {ApprovedSubmissions}", logPrefix, approvedSubmissions);
             return Ok(approvedSubmissions);
         }
         catch (TimeoutException ex)
         {
+            logger.LogError(ex, "{Logprefix}: SubmissionsController - GetApprovedSubmissionsWithAggregatedPomData: Get Approved Submissions request Timedout - Exception {Ex}", logPrefix, ex.Message);
             return StatusCode(StatusCodes.Status504GatewayTimeout, ex.Message);
         }
         catch (Exception ex)
         {
+            logger.LogError(ex, "{Logprefix}: SubmissionsController - GetApprovedSubmissionsWithAggregatedPomData: Get Approved Submissions request Failed - Exception {Error}", logPrefix, ex.Message);
             return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
         }
     }
--- src/EPR.CommonDataService.Core/Services/SubmissionsService.cs
+++ src/EPR.CommonDataService.Core/Services/SubmissionsService.cs
@@ -1,9 +1,13 @@
+using Azure.Core;
 using EPR.CommonDataService.Core.Extensions;
 using EPR.CommonDataService.Core.Models.Requests;
 using EPR.CommonDataService.Core.Models.Response;
 using EPR.CommonDataService.Data.Entities;
 using EPR.CommonDataService.Data.Infrastructure;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System.Data;
 
 namespace EPR.CommonDataService.Core.Services;
@@ -17,49 +21,66 @@ public interface ISubmissionsService
     Task<IList<ApprovedSubmissionEntity>> GetApprovedSubmissionsWithAggregatedPomData(DateTime approvedAfter, string periods);
 }
 
-public class SubmissionsService(
-    SynapseContext accountsDbContext,
-    IDatabaseTimeoutService databaseTimeoutService)
-    : ISubmissionsService
+public class SubmissionsService(SynapseContext accountsDbContext, IDatabaseTimeoutService databaseTimeoutService, ILogger<SubmissionsService> logger, IConfiguration config) : ISubmissionsService
 {
+    private readonly string? logPrefix = string.IsNullOrEmpty(config["LogPrefix"]) ? "[EPR.CommonDataService]" : config["LogPrefix"];
+
     public async Task<PaginatedResponse<PomSubmissionSummary>> GetSubmissionPomSummaries<T>(SubmissionsSummariesRequest<T> request)
     {
+        logger.LogInformation("{Logprefix}: SubmissionsService - GetSubmissionPomSummaries: Get Pom Submissions for given request {PomSubmissions}", logPrefix, JsonConvert.SerializeObject(request));
+
         var sql = "EXECUTE apps.sp_FilterAndPaginateSubmissionsSummaries @OrganisationName, @OrganisationReference, @RegulatorUserId, @StatusesCommaSeperated, @OrganisationType, @PageSize, @PageNumber, @DecisionsDelta, @SubmissionYearsCommaSeperated, @SubmissionPeriodsCommaSeperated, @ActualSubmissionPeriodsCommaSeperated";
+        logger.LogInformation("{Logprefix}: SubmissionsService - GetSubmissionPomSummaries: executing query {Sql}", logPrefix, sql);
 
         var sqlParameters = request.ToProcParams();
+        logger.LogInformation("{Logprefix}: SubmissionsService - GetSubmissionPomSummaries: query parameters {Parameters}", logPrefix, JsonConvert.SerializeObject(sqlParameters));
 
         var response = await accountsDbContext.RunSqlAsync<PomSubmissionSummaryRow>(sql, sqlParameters);
         var itemsCount = response.FirstOrDefault()?.TotalItems ?? 0;
+        var paginatedResponse = response.ToPaginatedResponse<PomSubmissionSummaryRow, T, PomSubmissionSummary>(request, itemsCount);
 
-        return response.ToPaginatedResponse<PomSubmissionSummaryRow, T, PomSubmissionSummary>(request, itemsCount);
+        logger.LogInformation("{Logprefix}: SubmissionsService - GetSubmissionPomSummaries: Sql query response {Sql}", logPrefix, JsonConvert.SerializeObject(paginatedResponse));
+        return paginatedResponse;
     }
 
     public async Task<PaginatedResponse<RegistrationSubmissionSummary>> GetSubmissionRegistrationSummaries<T>(SubmissionsSummariesRequest<T> request)
     {
+        logger.LogInformation("{Logprefix}: SubmissionsService - GetSubmissionRegistrationSummaries: Get Registration Submissions for given request {RegistrationSubmissions}", logPrefix, JsonConvert.SerializeObject(request));
+
         var sql = "EXECUTE apps.sp_FilterAndPaginateRegistrationsSummaries @OrganisationName, @OrganisationReference, @RegulatorUserId, @StatusesCommaSeperated, @OrganisationType, @PageSize, @PageNumber, @DecisionsDelta, @SubmissionYearsCommaSeperated, @ActualSubmissionPeriodsCommaSeperated";
+        logger.LogInformation("{Logprefix}: SubmissionsService - GetSubmissionRegistrationSummaries: executing query {Sql}", logPrefix, sql);
 
         var sqlParameters = request.ToProcParams();
+        logger.LogInformation("{Logprefix}: SubmissionsService - GetSubmissionRegistrationSummaries: query parameters {Parameters}", logPrefix, JsonConvert.SerializeObject(sqlParameters));
 
         var response = await accountsDbContext.RunSqlAsync<RegistrationsSubmissionSummaryRow>(sql, sqlParameters);
         var itemsCount = response.FirstOrDefault()?.TotalItems ?? 0;
+        var paginatedResponse = response.ToPaginatedResponse<RegistrationsSubmissionSummaryRow, T, RegistrationSubmissionSummary>(request, itemsCount);
 
-        return response.ToPaginatedResponse<RegistrationsSubmissionSummaryRow, T, RegistrationSubmissionSummary>(request, itemsCount);
+        logger.LogInformation("{Logprefix}: SubmissionsService - GetSubmissionRegistrationSummaries: Sql query response {Sql}", logPrefix, JsonConvert.SerializeObject(paginatedResponse));
+        return paginatedResponse;
     }
 
     public async Task<IList<ApprovedSubmissionEntity>> GetApprovedSubmissionsWithAggregatedPomData(DateTime approvedAfter, string periods)
     {
+        logger.LogInformation("{Logprefix}: SubmissionsService - GetApprovedSubmissionsWithAggregatedPomData: Get approved submissions after {ApprovedAfter}, for periods {Periods}", logPrefix, approvedAfter.ToString(), periods);
+
         var sql = "EXECUTE rpd.sp_GetApprovedSubmissionsWithAggregatedPomDataV2 @ApprovedAfter, @Periods";
+        logger.LogInformation("{Logprefix}: SubmissionsService - GetApprovedSubmissionsWithAggregatedPomData: executing query {Sql}", logPrefix, sql);
 
         try
         {
             databaseTimeoutService.SetCommandTimeout(accountsDbContext, 120);
-
-            return await accountsDbContext.RunSqlAsync<ApprovedSubmissionEntity>(sql,
+            var paginatedResponse = await accountsDbContext.RunSqlAsync<ApprovedSubmissionEntity>(sql,
                 new SqlParameter("@ApprovedAfter", SqlDbType.DateTime2) { Value = approvedAfter },
                 new SqlParameter("@Periods", SqlDbType.VarChar) { Value = periods ?? (object)DBNull.Value });
+
+            logger.LogInformation("{Logprefix}: SubmissionsService - GetApprovedSubmissionsWithAggregatedPomData: Sql query response {Sql}", logPrefix, JsonConvert.SerializeObject(paginatedResponse));
+            return paginatedResponse;
         }
         catch (Exception ex)
         {
+            logger.LogError(ex, "{Logprefix}: SubmissionsService - GetApprovedSubmissionsWithAggregatedPomData: An error occurred while accessing the database. - {Ex}", logPrefix, ex.Message);
             throw new DataException("An error occurred while accessing the database.", ex);
         }
     }
