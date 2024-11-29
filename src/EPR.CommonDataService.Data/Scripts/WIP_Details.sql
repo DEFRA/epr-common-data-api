c80efd9ccb8a6050086d3b7d16509d4e18ccd568
Julian Young <julian.young.external@eviden.com>
2024-11-29 02:48:44 +0000

    Added view for ComplianceSchemeMembership Added View for ProducerPayCal API Parameterisation Refined Details SP to use the ProducerPayCal View amongst other things

--- src/EPR.CommonDataService.Data/Scripts/WIP_Details.sql
+++ src/EPR.CommonDataService.Data/Scripts/WIP_Details.sql
@@ -10,11 +10,18 @@ BEGIN
 
 	DECLARE @OrganisationIDForSubmission INT;
 	DECLARE @OrganisationUUIDForSubmission UNIQUEIDENTIFIER;
-	SET @OrganisationIDForSubmission = (SELECT O.Id from [rpd].[Submissions] as S
+	DECLARE @SubmissionPeriod nvarchar(4000);
+	DECLARE @CSOReferenceNumber nvarchar(4000);
+	DECLARE @IsComplianceScheme bit;
+
+	SELECT  @OrganisationIDForSubmission = O.Id,
+			@OrganisationUUIDForSubmission = O.ExternalId,
+			@CSOReferenceNumber = O.ReferenceNumber,
+			@IsComplianceScheme = O.IsComplianceScheme,
+			@SubmissionPeriod = S.SubmissionId
+	FROM [rpd].[Submissions] as S
 		inner join [rpd].[Organisations] O on S.OrganisationId = O.ExternalId
-		WHERE S.SubmissionId = @SubmissionId);
-	SET @OrganisationUUIDForSubmission = (SELECT S.OrganisationId from [rpd].[Submissions] as S
-		WHERE S.SubmissionId = @SubmissionId);
+	WHERE S.SubmissionId = @SubmissionId;
 
     WITH
 		-- basic OrganisationInformation
@@ -46,80 +53,14 @@ BEGIN
 				submission.ProducerCommentDate
 				from [dbo].[v_OrganisationRegistrationSummaries] as submission where submission.SubmissionId = @SubmissionId
 		)
-		-- for paycal
-		,SubsidiaryCountsCTE AS (
-			SELECT 
-				o.organisation_id,
-				ISNULL(s.NumberOfSubsidiaries, 0) AS NumberOfSubsidiaries
-			FROM 
-				(SELECT DISTINCT organisation_id FROM rpd.companyDetails) o
-			LEFT JOIN 
-				(
-					SELECT 
-						organisation_id,
-						COUNT(*) AS NumberOfSubsidiaries
-					FROM 
-						rpd.companyDetails
-					WHERE 
-						subsidiary_id IS NOT NULL
-					GROUP BY 
-						organisation_id
-				) s
-			ON o.organisation_id = s.organisation_id
-		)
-		,OnlineMarketSubsidiaryCountCTE as (
-			SELECT 
-				o.organisation_id,
-				ISNULL(s.NumberOfSubsidiariesBeingOnlineMarketPlace, 0) AS NumberOfSubsidiariesBeingOnlineMarketPlace
-			FROM 
-				(SELECT DISTINCT organisation_id FROM rpd.companyDetails) o
-			LEFT JOIN 
-				(
-					SELECT 
-						organisation_id,
-						COUNT(*) AS NumberOfSubsidiariesBeingOnlineMarketPlace
-					FROM 
-						rpd.companyDetails
-					WHERE 
-						packaging_activity_om IN ('Primary', 'Secondary')
-					GROUP BY 
-						organisation_id
-				) s
-			ON 
-				o.organisation_id = s.organisation_id
-		)
-		,SubsidiaryCountCTE AS (
-			select organisation_id,
-				   NumberOfSubsidiaries
-			from SubsidiaryCountsCTE where organisation_id = @OrganisationIDForSubmission
-		)
-		,OnlineMarketplaceCountCTE AS (
-			select organisation_id, NumberOfSubsidiariesBeingOnlineMarketPlace
-			from OnlineMarketSubsidiaryCountCTE
-			where organisation_id = @OrganisationIDForSubmission
-		),
-		OrganisationMarketPlaceInformationCTE AS (
-			SELECT
-				@SubmissionId as SubmissionId,
-				cd.organisation_id,
-				cd.organisation_size AS ProducerSize,
-				CASE
-					WHEN EXISTS (
-						SELECT 1
-						FROM rpd.companyDetails
-						WHERE organisation_id = @OrganisationIDForSubmission
-						AND packaging_activity_om IN ('Primary', 'Secondary')
-					) THEN CAST(1 AS BIT)
-					ELSE CAST(0 AS BIT)
-				END AS IsOnlineMarketplace,
-				ISNULL(sc.NumberOfSubsidiaries,0) as NumberOfSubsidiaries,
-				ISNULL(omc.NumberOfSubsidiariesBeingOnlineMarketPlace,0) as NumberOfSubsidiariesBeingOnlineMarketPlace
-			FROM
-				rpd.Organisations org
-				LEFT JOIN rpd.companyDetails cd ON org.id = cd.organisation_id
-				LEFT JOIN SubsidiaryCountCTE sc ON sc.organisation_id = cd.organisation_id
-				LEFT JOIN OnlineMarketplaceCountCTE omc ON omc.organisation_id = cd.organisation_id
-			WHERE cd.organisation_id = @OrganisationIDForSubmission
+		,ProducerPaycalParametersCTE AS (
+			SELECT ExternalId
+				   , ProducerSize 
+				   , IsOnlineMarketplace
+				   , NumberOfSubsidiaries
+				   , NumberOfSubsidiariesBeingOnlineMarketPlace
+			FROM [dbo].[v_ProducerPaycalParameters] as ppp
+			WHERE ppp.ExternalId = @OrganisationUUIDForSubmission
 		)
 		,SubmissionOrganisationDetails as (
 			SELECT DISTINCT
@@ -147,11 +88,11 @@ BEGIN
 				submission.SubmittedUserId,
 				submission.RegulatorDecisionDate,
 				submission.ProducerCommentDate,
-				org.IsOnlineMarketplace,
-				org.NumberOfSubsidiaries,
-				org.NumberOfSubsidiariesBeingOnlineMarketPlace
+				ppp.IsOnlineMarketplace,
+				ppp.NumberOfSubsidiaries,
+				ppp.NumberOfSubsidiariesBeingOnlineMarketPlace
 				from SubmissionSummary as submission
-					inner join OrganisationMarketPlaceInformationCTE org on org.SubmissionId = submission.SubmissionId
+					left join ProducerPaycalParametersCTE ppp on ppp.ExternalId = submission.OrganisationId
 		)
 		 -- producer comments
 		,AllRelatedProducerCommentEventsCTE as (
@@ -224,7 +165,7 @@ BEGIN
 			where RowNum = 1 
 		)
 		-- Submission Details with Producer and Regulator Comments
-		,SubmissionOrganisationCommentDetailsCTE as (
+		,SubmissionOrganisationCommentsDetailsCTE as (
 			SELECT 
 				submission.SubmissionId,
 				submission.OrganisationId,
@@ -259,41 +200,174 @@ BEGIN
 				left join LatestRegulatorDecisionEventsCTE decision on decision.SubmissionId = submission.SubmissionId
 				left join LatestProducerCommentEventsCTE producer on producer.SubmissionId = submission.SubmissionId
 		)
-		,LatestCompanyDetailsCTE AS ( 
-			select NewID() as CompanyDetailsFileId,
-				   'CompanyDetailsFileName.csv' as CompanyDetailsFileName,
-				   'abc' as CompanyDetailsBlobName,
-					'xyz' as RegistrationSetId
+		, AllOrganisationFiles AS (
+			SELECT
+				FileId,
+				BlobName,
+				FileType,
+				OriginalFileName as FileName,
+				TargetDirectoryName,
+				RegistrationSetId,
+				OrganisationId,
+				SubmissionType,
+				load_ts,
+				ROW_NUMBER() OVER (
+					PARTITION BY submissionId 
+					ORDER BY load_ts DESC
+				) AS row_num
+			FROM
+				[rpd].[cosmos_file_metadata]
+				where FileType in ('Partnerships', 'Brands', 'CompanyDetails')
+				and SubmissionType = 'Registration'
+				and OrganisationId = @OrganisationUUIDForSubmission
+		),
+		AllBrandFiles as (
+			select FileId as BrandFileId, 
+					BlobName as BrandBlobName, 
+					FileType as BrandFileType, 
+					FileName as BrandFileName,
+					OrganisationId,
+					load_ts,
+					ROW_NUMBER() OVER (
+						PARTITION BY OrganisationId 
+						ORDER BY load_ts DESC
+					) AS row_num
+			from AllOrganisationFiles aof
+			where aof.FileType = 'Brands'
+		),
+		AllPartnershipFiles as (
+			select FileId as PartnerFileId, 
+					BlobName as PartnerBlobName, 
+					FileType as PartnerFileType, 
+					FileName as PartnerFileName,
+					OrganisationId,
+					load_ts,
+					ROW_NUMBER() OVER (
+						PARTITION BY OrganisationId
+						ORDER BY load_ts DESC
+					) AS row_num
+			from AllOrganisationFiles aof
+			where aof.FileType = 'Partnerships'
+		),
+		AllCompanyFiles as (
+			select FileId as CompanyFileId, 
+					BlobName as CompanyBlobName, 
+					FileType as CompanyFileType, 
+					FileName as CompanyFileName,
+					OrganisationId,
+					load_ts,
+					ROW_NUMBER() OVER (
+						PARTITION BY OrganisationId
+						ORDER BY load_ts DESC
+					) AS row_num
+			from AllOrganisationFiles aof
+			where aof.FileType = 'CompanyDetails'
+		),
+		LatestBrandsFile as (
+			select BrandFileId, BrandBlobName, BrandFileType, BrandFileName, OrganisationId
+			from AllBrandFiles abf
+			where abf.row_num = 1
+		),
+		LatestPartnerFile as (
+			select PartnerFileId, PartnerBlobName, PartnerFileType, PartnerFileName, OrganisationId
+			from AllPartnershipFiles apf
+			where apf.row_num = 1			   
+		),
+		LatestCompanyFiles as (
+			select CompanyFileId, CompanyBlobName, CompanyFileType, CompanyFileName, OrganisationId
+			from AllCompanyFiles acf
+			where acf.row_num = 1			   
 		)
-		,LatestBrandDetailsCTE AS(
-			select NewID() as BrandsFileId,
-				   'BrandsFileName.csv' as BrandsFileName,
-				   'def' as BrandsBlobName,
-				   'xyz' as RegistrationSetId
-		)
-		,LatestPartnershipDetailsCTE AS (
-			select NewID() as PartnershipFileId,
-				   'PartnershipFileName.csv' as PartnershipFileName,
-				   'ghi' as PartnershipBlobName,
-				   'xyz' as RegistrationSetId
+		,AllCombinedOrgFiles as (
+			SELECT
+				lcf.OrganisationId, CompanyFileId, CompanyFileName, CompanyBlobName, BrandFileId, BrandFileName, BrandBlobName, PartnerFileId, PartnerFileName, PartnerBlobName
+			FROM
+				LatestCompanyFiles lcf
+					left outer join LatestBrandsFile lbf
+						left outer join LatestPartnerFile lpf
+						on lpf.OrganisationId = lbf.OrganisationId
+					on lcf.OrganisationId = lbf.OrganisationId
 		)
 		,JoinDataWithPartnershipAndBrandsCTE AS (
 			SELECT
 				joinedSubmissions.*,
-				companyDetails.CompanyDetailsFileId,
-				companyDetails.CompanyDetailsFileName,
-				companyDetails.CompanyDetailsBlobName,
-				brands.BrandsFileName,
-				brands.BrandsFileId,
-				brands.BrandsBlobName,
-				partnerships.PartnershipFileName,
-				partnerships.PartnershipFileId,
-				partnerships.PartnershipBlobName
-			FROM SubmissionOrganisationCommentDetailsCTE AS joinedSubmissions
-			left JOIN LatestCompanyDetailsCTE companyDetails ON companyDetails.RegistrationSetId = 'xyz'
-			LEFT JOIN LatestBrandDetailsCTE brands ON brands.RegistrationSetId = companyDetails.RegistrationSetId
-			LEFT JOIN LatestPartnershipDetailsCTE partnerships ON partnerships.RegistrationSetId = brands.RegistrationSetId
+				CompanyFileId as CompanyDetailsFileId,
+				CompanyFileName as CompanyDetailsFileName,
+				CompanyBlobName as CompanyDetailsBlobName,
+				BrandFileId as BrandsFileId,
+				BrandFileName as BrandsFileName,
+				BrandBlobName BrandsBlobName,
+				PartnerFileName as PartnershipFileName,
+				PartnerFileId as PartnershipFileId,
+				PartnerBlobName as PartnershipBlobName
+			FROM SubmissionOrganisationCommentsDetailsCTE AS joinedSubmissions
+			left join AllCombinedOrgFiles acof on acof.OrganisationId = joinedSubmissions.OrganisationId
 		)
+		,AllOrgFilesCTE as (
+			SELECT distinct
+				c.[OrganisationId]
+				,o.ExternalId
+				,c.[FileName]
+				,c.[FileType]
+				,c.submissionperiod as submission_period_desc
+				,c.created
+				--For a given Organisation, in a given submission period, finding the most recently accepted org file based on the submission date--
+				,Row_Number() Over(Partition by c.organisationid
+												,c.submissionperiod 
+											order by CONVERT(DATETIME, Substring(c.[created], 1, 23))  desc) 
+								as RowNumber
+			FROM rpd.organisations o
+			INNER JOIN [rpd].[cosmos_file_metadata] c ON c.organisationid = o.externalid AND FileType = 'CompanyDetails'
+			WHERE o.IsComplianceScheme = 1
+		)
+		,LatestOrgFileCTE as (
+			SELECT 
+			ExternalId,
+			OrganisationId,
+			FileName,
+			FileType,
+			submission_period_desc,
+			created
+			from AllOrgFilesCTE
+			where RowNumber = 1
+		) -- we don't need accepted Files, our journey is for the Regulator to Accept or Reject the files
+		,Accepted_CSO_org_files AS (
+			SELECT distinct
+				c.[OrganisationId]
+				,o.[ExternalId]
+				,c.[FileName]
+				,c.[FileType]
+				,c.submissionperiod as submission_period_desc
+				,c.created
+				--For a given Organisation, in a given submission period, finding the most recently accepted Pom file based on the submission date--
+				,Row_Number() Over(Partition by c.organisationid
+												, c.submissionperiod 
+								   order by CONVERT(DATETIME, Substring(c.[created], 1, 23))  desc) 
+							   as RowNumber
+				FROM rpd.organisations o
+				INNER JOIN [rpd].[cosmos_file_metadata] c ON c.organisationid = o.externalid
+				INNER JOIN [rpd].[submissionevents] se ON Trim(se.fileid) = Trim(c.fileid)
+														AND se.[type] = 'RegulatorRegistrationDecision'
+														AND se.decision = 'Accepted'
+														AND Trim(c.filetype) = 'CompanyDetails'
+		)
+		,Latest_Accepted_CSO_Org_file as (
+			--From the CTE retrieve the Filename of the identified file--
+			SELECT DISTINCT ExternalId, OrganisationId, Filename,FileType,submission_period_desc,created
+			FROM Accepted_CSO_org_files acof
+			WHERE acof.RowNumber = 1
+		)
+		,Unaccepted_MemberOrgsCTE as (
+			SELECT DISTINCT lofc.ExternalId, organisation_id as OrganisationId
+			from [rpd].[CompanyDetails] cd
+			inner join LatestOrgFileCTE lofc on lofc.FileName = cd.FileName
+		)
+		,Accepted_MemberOrgsCTE as (
+			SELECT DISTINCT laofc.ExternalId, organisation_id as OrganisationId
+			from [rpd].[CompanyDetails] cd
+			inner join Latest_Accepted_CSO_Org_file laofc on laofc.FileName = cd.FileName
+		)
+
 	SELECT
 		r.SubmissionId,
 		r.OrganisationId,
