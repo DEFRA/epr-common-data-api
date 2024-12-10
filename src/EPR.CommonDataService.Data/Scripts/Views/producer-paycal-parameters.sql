13361cdb3fb74ae55201b1b92bbed185dda35ad6
Julian Young <julian.young.external@eviden.com>
2024-12-10 12:20:43 +0000

    Corrected ProducerPayCal Parameters view Applied change for Organisation Details Summary view

--- src/EPR.CommonDataService.Data/Scripts/Views/producer-paycal-parameters.sql
+++ src/EPR.CommonDataService.Data/Scripts/Views/producer-paycal-parameters.sql
@@ -8,71 +8,95 @@ GO
 
 CREATE VIEW [dbo].v_ProducerPaycalParameters 
 as 
-WITH SubsidiaryCountsCTE AS (
-	SELECT 
-		organisation_id as OrganisationReference,
-		COUNT(DISTINCT subsidiary_id) AS NumberOfSubsidiaries
-	FROM rpd.companydetails cd
-	where organisation_id is not null
-	GROUP BY organisation_id
-),
-OnlineMarketSubsidiaryCountCTE as (
-	SELECT 
-		organisation_id as OrganisationReference,
-		COUNT(DISTINCT subsidiary_id) AS NumberOfSubsidiariesBeingOnlineMarketPlace
-	FROM rpd.companydetails cd
-	where subsidiary_id is not null
-		  and packaging_activity_om IN ('Primary', 'Secondary')
-    GROUP BY organisation_id
-)
-,SubsidiaryAndMarketPlaceCountsCTE as (
-    SELECT ms.OrganisationReference,
-        sc.NumberOfSubsidiaries,
-        ms.NumberOfSubsidiariesBeingOnlineMarketPlace
-    from OnlineMarketSubsidiaryCountCTE as ms
-        inner join SubsidiaryCountsCTE as sc on sc.OrganisationReference = ms.OrganisationReference
-),
-OrganisationSizesCTE as (
-    SELECT DISTINCT organisation_id,
-        CASE
-            organisation_size
-            WHEN 'L' then 'large'
-            WHEN 'S' then 'small'
-            else organisation_size
-        END as organisation_size,
-        ROW_NUMBER() OVER (
-            PARTITION BY organisation_id
-            ORDER BY cd.load_ts DESC -- mark latest submissionEvent synced from cosmos
-        ) as RowNum
-    from rpd.CompanyDetails cd
-    where organisation_id is not null
-),
-MostRecentOrganisationSizeCTE as (
-    SELECT distinct organisation_id as OrganisationReference,
-        organisation_size as OrganisationSize
-    from OrganisationSizesCTE cd
-    where RowNum = 1
-),
-OrganisationMarketPlaceInformationCTE AS (
-    SELECT 
-		o.ExternalId,
-		smp.OrganisationReference,
-        OrganisationSize AS ProducerSize,
-        CASE
-            WHEN EXISTS (
-                SELECT 1
-                FROM rpd.companyDetails
-                WHERE organisation_id = smp.OrganisationReference
-                    AND packaging_activity_om IN ('Primary', 'Secondary')
-            ) THEN CAST(1 AS BIT)
-            ELSE CAST(0 AS BIT)
-        END AS IsOnlineMarketplace,
-        ISNULL(smp.NumberOfSubsidiaries, 0) as NumberOfSubsidiaries,
-        ISNULL(smp.NumberOfSubsidiariesBeingOnlineMarketPlace, 0) as NumberOfSubsidiariesBeingOnlineMarketPlace
-    FROM SubsidiaryAndMarketPlaceCountsCTE as smp
-        left join MostRecentOrganisationSizeCTE mros ON mros.OrganisationReference = smp.OrganisationReference
-	LEFT JOIN rpd.Organisations o on o.ReferenceNumber = smp.OrganisationReference
-)
-select *
-from OrganisationMarketPlaceInformationCTE;
-go
+WITH 
+    SubsidiaryCountsCTE
+    AS
+    (
+        SELECT
+            organisation_id AS OrganisationReference
+            ,COUNT(DISTINCT subsidiary_id) AS NumberOfSubsidiaries
+        FROM
+            rpd.companydetails cd
+        WHERE organisation_id IS NOT NULL
+        GROUP BY organisation_id
+    )
+    ,OnlineMarketSubsidiaryCountCTE
+    AS
+    (
+        SELECT
+            organisation_id AS OrganisationReference
+            ,COUNT(DISTINCT subsidiary_id) AS NumberOfSubsidiariesBeingOnlineMarketPlace
+        FROM
+            rpd.companydetails cd
+        WHERE subsidiary_id IS NOT NULL
+            AND UPPER(packaging_activity_om) IN ('PRIMARY', 'SECONDARY')
+        GROUP BY organisation_id
+    )
+	,SubsidiaryAndMarketPlaceCountsCTE
+    AS
+    (
+        SELECT
+            ms.OrganisationReference
+            ,sc.NumberOfSubsidiaries
+            ,ms.NumberOfSubsidiariesBeingOnlineMarketPlace
+        FROM
+            OnlineMarketSubsidiaryCountCTE AS ms
+            INNER JOIN SubsidiaryCountsCTE AS sc ON sc.OrganisationReference = ms.OrganisationReference
+    )
+    ,OrganisationSizesCTE
+    AS
+    (
+        SELECT
+            DISTINCT
+            organisation_id
+            ,CASE
+				UPPER(organisation_size)
+				WHEN 'L' THEN 'large'
+				WHEN 'S' THEN 'small'
+				ELSE organisation_size
+			END AS organisation_size
+			,CASE UPPER(packaging_activity_om)
+				WHEN 'SECONDARY' THEN 1
+				WHEN 'PRIMARY' THEN 1
+				ELSE 0
+			END AS IsOnlineMarketPlace        
+			,ROW_NUMBER() OVER (
+				PARTITION BY organisation_id
+				ORDER BY cd.load_ts DESC
+			) AS RowNum
+        FROM
+            rpd.CompanyDetails cd
+        WHERE organisation_id IS NOT NULL AND subsidiary_id IS NULL
+    )
+    ,MostRecentOrganisationSizeCTE
+    AS
+    (
+        SELECT
+            DISTINCT
+            organisation_id AS OrganisationReference
+            ,organisation_size AS OrganisationSize
+			,IsOnlineMarketplace
+        FROM
+            OrganisationSizesCTE cd
+        WHERE RowNum = 1
+    )
+    ,OrganisationMarketPlaceInformationCTE
+    AS
+    (
+        SELECT
+            o.ExternalId
+            ,smp.OrganisationReference
+            ,OrganisationSize AS ProducerSize
+            ,IsOnlineMarketplace
+            ,ISNULL(smp.NumberOfSubsidiaries, 0) AS NumberOfSubsidiaries
+            ,ISNULL(smp.NumberOfSubsidiariesBeingOnlineMarketPlace, 0) AS NumberOfSubsidiariesBeingOnlineMarketPlace
+        FROM
+            SubsidiaryAndMarketPlaceCountsCTE AS smp
+            INNER JOIN MostRecentOrganisationSizeCTE mros ON mros.OrganisationReference = smp.OrganisationReference
+            INNER JOIN rpd.Organisations o ON o.ReferenceNumber = smp.OrganisationReference
+    )
+SELECT
+    *
+FROM
+    OrganisationMarketPlaceInformationCTE;
+GO
