1213278facb6cfe283d0ad875458c83a559a2dd5
Salman Qayyum (Eviden) <Salman.Qayyum@Defra.onmicrosoft.com>
2024-12-13 21:57:59 +0000

    fixed store procedure changes

--- src/EPR.CommonDataService.Data/Scripts/Stored Procedures/get-cso-member-details-by-organisationid.sql
+++ src/EPR.CommonDataService.Data/Scripts/Stored Procedures/get-cso-member-details-by-organisationid.sql
@@ -38,26 +38,41 @@ SubsidiaryCount AS (
         AND CD.subsidiary_id IS NOT NULL
     GROUP BY 
         CD.organisation_id
-)
+),
+OnlineMarketPlace AS (
+    SELECT 
+        CD.organisation_id, 
+        CASE WHEN  cd.packaging_activity_om IN ('Primary', 'Secondary') THEN 1 ELSE 0 END AS IsOnlineMarketPlace,
+		 cd.organisation_size as MemberType
+    FROM  
+        [rpd].[CompanyDetails] CD
+    WHERE EXISTS (
+            SELECT 1
+            FROM LatestFile LF
+            WHERE LTRIM(RTRIM(CD.[filename])) = LF.LatestFileName
+        )
+        AND CD.subsidiary_id IS NULL
+    GROUP BY 
+        CD.organisation_id,
+		cd.organisation_size,
+		CD.packaging_activity_om
+) 
 SELECT  COUNT(CASE WHEN  CD.subsidiary_id IS NOT NULL AND cd.packaging_activity_om IN ('Primary', 'Secondary') THEN 1 END) AS NumberOfSubsidiariesBeingOnlineMarketPlace,
     cd.organisation_id AS MemberId,
-    CAST(
-        CASE 
-            WHEN cd.packaging_activity_om IN ('Primary', 'Secondary') THEN 1
-            ELSE 0
-        END AS BIT
-    ) AS IsOnlineMarketplace,
-    cd.organisation_size AS MemberType,
-     ISNull( sc.NumberOfSubsidiaries,0) as NumberOfSubsidiaries
+    OMP.MemberType,
+     ISNull( sc.NumberOfSubsidiaries,0) as NumberOfSubsidiaries,
+	 	CAST(OMP.IsOnlineMarketPlace AS BIT) AS IsOnlineMarketplace
 FROM LatestFile LF
 INNER JOIN [rpd].[CompanyDetails] cd ON Trim(cd.[filename]) = Trim(LF.LatestFileName)
 LEFT JOIN SubsidiaryCount sc ON sc.organisation_id = cd.organisation_id
-GROUP BY 
-    cd.packaging_activity_om, 
-    cd.organisation_id,
-    cd.organisation_size,
-    sc.NumberOfSubsidiaries;
+LEFT JOIN OnlineMarketPlace OMP ON OMP.organisation_id = cd.organisation_id
 
+GROUP BY 
+    cd.organisation_id,
+	OMP.IsOnlineMarketPlace,
+	OMP.MemberType,
+    sc.NumberOfSubsidiaries;
+    
 END;
 
 GO
