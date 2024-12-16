297ddc9252f2de5624d4c60ceb262dab8b53fe94
SalmanQayyumEviden <salman.qayyum.external@eviden.com>
2024-12-16 12:19:19 +0000

    fixed soner cube warning (#80)
    Co-authored-by: Salman Qayyum (Eviden) <Salman.Qayyum@Defra.onmicrosoft.com>

--- src/EPR.CommonDataService.Core/Services/ProducerDetailsService.cs
+++ src/EPR.CommonDataService.Core/Services/ProducerDetailsService.cs
@@ -1,9 +1,9 @@
 using System.Data;
+using EPR.CommonDataService.Core.Mapper;
 using EPR.CommonDataService.Core.Models.Response;
 using EPR.CommonDataService.Data.Entities;
 using EPR.CommonDataService.Data.Infrastructure;
 using Microsoft.Data.SqlClient;
-using static EPR.CommonDataService.Core.Mapper.ProducerDetailsService;
 
 namespace EPR.CommonDataService.Core.Services;
 
