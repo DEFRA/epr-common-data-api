ee325e63d1685d5463954db1172a0c2004cb4b75
prasad.ramireddy@defra.onmicrosoft.com <prasad.ramireddy@defra.onmicrosoft.com>
2025-02-19 11:26:59 +0000

    Minor change to add new filters for Resubmission statues

--- src/EPR.CommonDataService.Core/Models/Requests/OrganisationRegistrationFilterRequest.cs
+++ src/EPR.CommonDataService.Core/Models/Requests/OrganisationRegistrationFilterRequest.cs
@@ -7,15 +7,22 @@ namespace EPR.CommonDataService.Core.Models.Requests
     public class OrganisationRegistrationFilterRequest : IPaginatedRequest
     {
         public string? OrganisationNameCommaSeparated { get; set; }
+
         public string? OrganisationIDCommaSeparated { get; set; }
+
         public string? RelevantYearCommaSeparated { get; set; }
+
         public string? SubmissionStatusCommaSeparated { get; set; }
+
+        public string? ResubmissionStatusCommaSeparated { get; set; }
+
         public string? OrganisationTypesCommaSeparated { get; set; }
 
         public string? ApplicationReferenceNumbers { get; set; }
 
         [Required]
         public int PageNumber { get; set; }
+
         [Required]
         public int PageSize { get; set; }
     }
