ce6198c6e0f700b928e3a1aa504b153caaf5f61e
Julian Young <julian.young.external@eviden.com>
2025-03-25 11:00:57 +0000

    Moved LastSyncTime to an SP.  Both in the Commit/

--- src/EPR.CommonDataService.Core/Services/SubmissionEventService.cs
+++ src/EPR.CommonDataService.Core/Services/SubmissionEventService.cs
@@ -1,6 +1,7 @@
 using EPR.CommonDataService.Core.Models;
 using EPR.CommonDataService.Data.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EPR.CommonDataService.Core.Services;
 
@@ -10,15 +11,28 @@ public interface ISubmissionEventService
 }
 
 public class SubmissionEventService(
-    SynapseContext accountsDbContext) 
+    SynapseContext accountsDbContext, ILogger<SubmissionsService> logger) 
     : ISubmissionEventService
 {
-    public async Task<SubmissionEventsLastSync> GetLastSyncTimeAsync()
+    public async Task<SubmissionEventsLastSync> GetLastSyncTimeAsyncOld()
     {
         var lastSyncTime =  await accountsDbContext.SubmissionEvents.MaxAsync(se => se.LastSyncTime);
         return new SubmissionEventsLastSync
         {
             LastSyncTime = lastSyncTime
         };
+    }
+
+    public async Task<SubmissionEventsLastSync> GetLastSyncTimeAsync()
+    {
+        var sql = "dbo.GetLastSyncTime";
+
+        var response = await accountsDbContext.RunSpCommandAsync<SubmissionEventsLastSync>(sql, logger, "GetLastSyncTime", []);
+        if ( response.Count == 0 )
+        {
+            Exception exception = new("No data found from GetLastSyncTime");
+            throw exception;
+        }
+        return response[0];
     }
 }
