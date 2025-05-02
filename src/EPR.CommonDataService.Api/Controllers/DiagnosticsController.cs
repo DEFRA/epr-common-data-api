2d79eae6d40a833f624d583795c39f256b7bc4ed
Karthik K V <karthik.vishwanathan.external@eviden.com>
2025-05-02 10:41:37 +0100

    Initial diagnostics

---
+++ src/EPR.CommonDataService.Api/Controllers/DiagnosticsController.cs
@@ -0,0 +1,38 @@
+﻿using EPR.CommonDataService.Api.Configuration;
+using EPR.CommonDataService.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.CommonDataService.Api.Controllers
+{
+    [ExcludeFromCodeCoverage]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiagnosticsController(IDiagnosticsService diagService, IOptions<ApiConfig> apiConfig, ILogger<SubmissionsController> logger, IConfiguration config) : ControllerBase
+    {
+        private readonly string? _logPrefix = string.IsNullOrEmpty(config["LogPrefix"]) ? "[EPR.CommonDataService]" : config["LogPrefix"];
+        private readonly ApiConfig apiConfig = apiConfig.Value;
+
+        [HttpGet("diag/CompSchemeById")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetComplianceSchemeInfo(string? SubmissionId, string? ComplianceSchemeId )
+        {
+            var objRet = await diagService.GetComplianceScheme(SubmissionId, ComplianceSchemeId);
+            return Ok(objRet);
+        }
+
+        [HttpGet("diag/CompSchemeMembersById")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetComplianceSchemeMembers(string? SubmissionId, string? ComplianceSchemeId)
+        {
+            var objRet = await diagService.GetComplianceSchemeMembersById(SubmissionId, ComplianceSchemeId);
+            return Ok(objRet);
+        }
+    }
+}
