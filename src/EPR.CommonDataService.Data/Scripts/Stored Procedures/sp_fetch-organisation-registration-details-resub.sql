ef0383e4dac0e0befe200b499d0bbbda9cb73446
Julian Young <julian.young.external@eviden.com>
2025-05-15 18:34:39 +0100

    Corrected date assignment issue...

--- src/EPR.CommonDataService.Data/Scripts/Stored Procedures/sp_fetch-organisation-registration-details-resub.sql
+++ src/EPR.CommonDataService.Data/Scripts/Stored Procedures/sp_fetch-organisation-registration-details-resub.sql
@@ -15,12 +15,7 @@ SET NOCOUNT ON;
 	DECLARE @ComplianceSchemeId nvarchar(50);
 	DECLARE @ApplicationReferenceNumber nvarchar(4000);
 	DECLARE @IsComplianceScheme bit;
-
-    DECLARE @LateFeeCutoffDate DATETIME = DATEFROMPARTS(CONVERT( int, SUBSTRING(
-                                        @SubmissionPeriod,
-                                        PATINDEX('%[0-9][0-9][0-9][0-9]', @SubmissionPeriod),
-                                        4
-                                    )),4, 1); 
+    DECLARE @LateFeeCutoffDate DATE; 
 
     SELECT
         @OrganisationIDForSubmission = O.Id 
@@ -34,6 +29,12 @@ SET NOCOUNT ON;
         [rpd].[Submissions] AS S
         INNER JOIN [rpd].[Organisations] O ON S.OrganisationId = O.ExternalId
     WHERE S.SubmissionId = @SubmissionId;
+
+	SET @LateFeeCutoffDate = DATEFROMPARTS(CONVERT( int, SUBSTRING(
+                                @SubmissionPeriod,
+                                PATINDEX('%[0-9][0-9][0-9][0-9]', @SubmissionPeriod),
+                                4
+                            )),4, 1);
 
     WITH
 		SubmissionEventsCTE as (
