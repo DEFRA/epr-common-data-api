f1d01252f864260d50ca85ba84f07e25c74177be
Julian Young <julian.young.external@eviden.com>
2025-07-30 10:41:51 +0100

    Remove unwanted timesout changes

--- src/EPR.CommonDataService.Api/Extensions/ServiceProviderExtensions.cs
+++ src/EPR.CommonDataService.Api/Extensions/ServiceProviderExtensions.cs
@@ -9,7 +9,6 @@ namespace EPR.CommonDataService.Api.Extensions;
 [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute]
 public static class ServiceProviderExtensions
 {
-    private const int NOMINAL_MAX_CMD_TIMEOUT = 3600; // seconds
     private const string BaseProblemTypePath = "ApiConfig:BaseProblemTypePath";
     public static IServiceCollection RegisterWebComponents(this IServiceCollection services, IConfiguration configuration)
     {
@@ -22,42 +21,9 @@ public static class ServiceProviderExtensions
 
     public static IServiceCollection RegisterDataComponents(this IServiceCollection services, IConfiguration configuration)
     {
-        string? connectionString = configuration.GetConnectionString("SynapseDatabase");
-        int timeoutInSeconds = extractTimeoutFromConnStr(connectionString) ?? configuration.GetValue<int?>("CommandTimeoutSeconds") ?? NOMINAL_MAX_CMD_TIMEOUT;
+        services.AddDbContext<SynapseContext>(options => options.UseSqlServer(configuration.GetConnectionString("SynapseDatabase")));
 
-        services.AddDbContext<SynapseContext>(options => options.UseSqlServer(connectionString,
-                                                         sqloptions => sqloptions.CommandTimeout(timeoutInSeconds)));
         return services;
-    }
-
-    private static int? extractTimeoutFromConnStr(string? connectionString)
-    {
-        static int? extractCommandTimeout(string? connStr)
-        {
-            const string key = "Command Timeout=";
-
-            if (string.IsNullOrWhiteSpace(connStr))
-                return null;
-
-            // Find the key (case-insensitive)
-            int idx = connStr.IndexOf(key, StringComparison.OrdinalIgnoreCase);
-            if (idx < 0)
-                return null;
-
-            // Move to the start of the numeric value
-            idx += key.Length;
-
-            // Find end of value (the next semicolon), or use end-of-string
-            int endIdx = connStr.IndexOf(';', idx);
-            string numStr = (endIdx >= 0)
-                ? connStr.Substring(idx, endIdx - idx)
-                : connStr.Substring(idx);
-
-            // Try to parse and return, else null
-            return int.TryParse(numStr, out var n) ? (int?)n : null;
-        }
-
-        return extractCommandTimeout(connectionString);
     }
 
     private static void AddControllers(IServiceCollection services, IConfiguration configuration)
