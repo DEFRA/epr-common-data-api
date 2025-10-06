8842d4f12370c4a8aefadd9132db00cc27288d21
Karthik K V <karthik.vishwanathan.external@atos.net>
2025-10-06 14:12:11 +0100

    update v_ProducerPaycalParameters_resub and add migration script t_ProducerPaycalParameters_resub for new column

---
+++ src/EPR.CommonDataService.Data/Migrations/t_ProducerPaycalParameters_resub.sql
@@ -0,0 +1,11 @@
+﻿-- NumberOfLateSubsidiaries for Direct Producer
+IF NOT EXISTS (
+    SELECT *
+    FROM sys.columns 
+    WHERE Name = N'NumberOfLateSubsidiaries' 
+    AND Object_ID = Object_ID(N'dbo.t_ProducerPaycalParameters_resub')
+)
+BEGIN
+ALTER TABLE dbo.t_ProducerPaycalParameters_resub
+    ADD [NumberOfLateSubsidiaries] [int] NULL
+END
