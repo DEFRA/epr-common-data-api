f34974944099160058b8bf14f8962a4465e1f010
Jayakrishnan Appusamy <jayakrishnan.appusamy@atos.net>
2025-10-21 16:53:14 +0100

    Updated based on review comments

--- src/EPR.CommonDataService.Data/Scripts/Stored Procedures/sp_ValidateOrganisationsObligatedDecisionFromPOMFiles.sql
+++ src/EPR.CommonDataService.Data/Scripts/Stored Procedures/sp_ValidateOrganisationsObligatedDecisionFromPOMFiles.sql
@@ -1,60 +1,42 @@
 IF EXISTS (SELECT 1 FROM sys.procedures WHERE object_id = OBJECT_ID(N'[dbo].[sp_ValidateOrganisationsObligatedDecisionFromPOMFiles]'))
-DROP PROCEDURE [dbo].[sp_ValidateOrganisationsObligatedDecisionFromPOMFiles];
+    DROP PROCEDURE [dbo].[sp_ValidateOrganisationsObligatedDecisionFromPOMFiles];
 GO
 
-CREATE PROC [dbo].[sp_ValidateOrganisationsObligatedDecisionFromPOMFiles] @ParamList [NVARCHAR](MAX) AS
+CREATE PROC [dbo].[sp_ValidateOrganisationsObligatedDecisionFromPOMFiles]
+(
+    @OrganisationIds NVARCHAR(MAX)
+)
+AS
 BEGIN
-    SET NOCOUNT ON;	
+    SET NOCOUNT ON;
+    
+	IF OBJECT_ID('tempdb..#DpOrganisationIdTable') IS NOT NULL
+        DROP TABLE #DpOrganisationIdTable;
 
-    -- Drop temp table if it exists
-    IF OBJECT_ID('tempdb..#OrgAndSubIdsList') IS NOT NULL
-        DROP TABLE #OrgAndSubIdsList;
+    IF OBJECT_ID('tempdb..#SubOrganisationIdTable') IS NOT NULL
+        DROP TABLE #SubOrganisationIdTable;
 
-    -- Create temp table to hold parsed values
-    CREATE TABLE #OrgAndSubIdsList (
-        ParamId NVARCHAR(10)
-    );
+    DECLARE @Delimiter CHAR(1) = ',';
+    
+    SELECT  DISTINCT CD.organisation_id AS OrganisationId
+    INTO    #DpOrganisationIdTable
+    FROM    rpd.CompanyDetails CD 
+            INNER JOIN (SELECT TRY_CAST(value AS NVARCHAR) AS OrgId FROM STRING_SPLIT(@OrganisationIds, @Delimiter)) I
+                                ON CD.organisation_id = I.OrgId OR CD.subsidiary_id = I.OrgId AND I.OrgId IS NOT NULL
+    WHERE   CD.subsidiary_id IS NULL
 
-    -- Split the input string and insert valid integers
-    INSERT INTO #OrgAndSubIdsList (ParamId)
-		SELECT TRY_CAST(value AS NVARCHAR)
-    FROM STRING_SPLIT(@ParamList, ',')
-    WHERE TRY_CAST(value AS NVARCHAR) IS NOT NULL;
-
-    -- Return the result set 
-	IF OBJECT_ID('tempdb..#DpOrganisationIdTable') IS NOT NULL DROP TABLE #DpOrganisationIdTable;
-    CREATE TABLE #DpOrganisationIdTable (OrganisationId NVARCHAR(10));
-
-    IF OBJECT_ID('tempdb..#SubOrganisationIdTable') IS NOT NULL DROP TABLE #SubOrganisationIdTable;
-    CREATE TABLE #SubOrganisationIdTable (OrganisationId NVARCHAR(10));
-
-	INSERT INTO #DpOrganisationIdTable (OrganisationId)
-		SELECT Distinct CD.organisation_id as OrganisationId
-	FROM [rpd].[CompanyDetails] CD 
-		INNER JOIN #OrgAndSubIdsList ids ON CD.organisation_id = ids.ParamId OR CD.subsidiary_id = CAST(ids.ParamId AS NVARCHAR)
-	WHERE CD.subsidiary_id IS NULL
-
-	INSERT INTO #SubOrganisationIdTable (OrganisationId)
-		SELECT CD.subsidiary_id AS OrganisationId
-	FROM rpd.CompanyDetails AS CD
-    WHERE
-        EXISTS (SELECT 1 FROM #DpOrganisationIdTable DIT WHERE DIT.OrganisationId = CD.organisation_id)
-        AND CD.subsidiary_id IS NOT NULL
+    SELECT  CD.subsidiary_id AS OrganisationId
+    INTO    #SubOrganisationIdTable
+    FROM    rpd.CompanyDetails AS CD
+    WHERE   EXISTS (SELECT 1 FROM #DpOrganisationIdTable DIT WHERE DIT.OrganisationId = CD.organisation_id)
+            AND CD.subsidiary_id IS NOT NULL
     GROUP BY CD.subsidiary_id
 
-	  -- Variables to hold the final results
-    DECLARE @DpOrgList NVARCHAR(MAX);
-    DECLARE @SubOrgList NVARCHAR(MAX);
-    
-    SELECT @DpOrgList = STRING_AGG(CAST(OrganisationId AS NVARCHAR(MAX)), ',')
-    FROM #DpOrganisationIdTable;
+    -- Variables to hold the final results
+    DECLARE @DpOrgList  NVARCHAR(MAX) = (SELECT STRING_AGG(CAST(OrganisationId AS NVARCHAR(MAX)), @Delimiter) FROM #DpOrganisationIdTable);
+    DECLARE @SubOrgList NVARCHAR(MAX) = (SELECT STRING_AGG(CAST(OrganisationId AS NVARCHAR(MAX)), @Delimiter) FROM #SubOrganisationIdTable);
 
-    SELECT @SubOrgList = STRING_AGG(CAST(OrganisationId AS NVARCHAR(MAX)), ',')
-    FROM #SubOrganisationIdTable;
-	
-	-- Call the child procedure
-	EXEC dbo.sp_ProcessOrganisationsToRetrieveObligationDecision		
-        @DpOrgIds = @DpOrgList,
-        @SubOrgIds = @SubOrgList;
+    -- Call the child procedure
+    EXEC dbo.sp_ProcessOrganisationsToRetrieveObligationDecision @DpOrgIds = @DpOrgList, @SubOrgIds = @SubOrgList;
 END;
 GO
--- src/EPR.CommonDataService.Data/Scripts/Stored Procedures/sp_ValidateOrganisationsObligatedDecisionFromRegistrationFile.sql
+++ src/EPR.CommonDataService.Data/Scripts/Stored Procedures/sp_ValidateOrganisationsObligatedDecisionFromRegistrationFile.sql
@@ -1,64 +1,43 @@
 IF EXISTS (SELECT 1 FROM sys.procedures WHERE object_id = OBJECT_ID(N'[dbo].[sp_ValidateOrganisationsObligatedDecisionFromRegistrationFile]'))
-DROP PROCEDURE [dbo].[sp_ValidateOrganisationsObligatedDecisionFromRegistrationFile];
+    DROP PROCEDURE [dbo].[sp_ValidateOrganisationsObligatedDecisionFromRegistrationFile];
 GO
 
-CREATE PROC [dbo].[sp_ValidateOrganisationsObligatedDecisionFromRegistrationFile] @FileIds [NVARCHAR](MAX) AS
+CREATE PROC [dbo].[sp_ValidateOrganisationsObligatedDecisionFromRegistrationFile] 
+(
+    @FileIds NVARCHAR(MAX) 
+)
+AS
 BEGIN
     SET NOCOUNT ON;
 
-    IF OBJECT_ID('tempdb..#FileIdTable') IS NOT NULL
-        DROP TABLE #FileIdTable;
-    CREATE TABLE #FileIdTable (FileId NVARCHAR(40));
-    
-    DECLARE @Delimiter CHAR(1) = ',';
-        
-    WITH Split_FileIds AS (
-        SELECT value AS FileId FROM STRING_SPLIT(@FileIds, @Delimiter)
-    )
-    INSERT INTO #FileIdTable (FileId)
-    SELECT FileId FROM Split_FileIds;
-
-    IF OBJECT_ID('tempdb..#DpOrganisationIdTable') IS NOT NULL
+	IF OBJECT_ID('tempdb..#DpOrganisationIdTable') IS NOT NULL
         DROP TABLE #DpOrganisationIdTable;
-    CREATE TABLE #DpOrganisationIdTable (OrganisationId NVARCHAR(10));
-
     IF OBJECT_ID('tempdb..#SubOrganisationIdTable') IS NOT NULL
         DROP TABLE #SubOrganisationIdTable;
-    CREATE TABLE #SubOrganisationIdTable (OrganisationId NVARCHAR(10));
 
-    INSERT INTO #DpOrganisationIdTable (OrganisationId)
-    SELECT CD.organisation_id
-    FROM rpd.SubmissionEvents AS SE
-    INNER JOIN rpd.cosmos_file_metadata AS CFM ON CFM.FileId = SE.FileId
-    INNER JOIN rpd.CompanyDetails AS CD ON CD.FileName = CFM.FileName
-    INNER JOIN #FileIdTable F ON F.FileId = SE.FileId
-    WHERE
-        CD.subsidiary_id IS NULL
-    GROUP BY
-        CD.organisation_id;
+    DECLARE @Delimiter CHAR(1) = ',';
 
-    INSERT INTO #SubOrganisationIdTable (OrganisationId)
-    SELECT CD.subsidiary_id
-    FROM rpd.CompanyDetails AS CD
-    INNER JOIN #DpOrganisationIdTable DID ON DID.OrganisationId = CD.organisation_id
-    WHERE
-        CD.subsidiary_id IS NOT NULL
-    GROUP BY
-        CD.subsidiary_id;
+    SELECT  CD.organisation_id AS OrganisationId
+    INTO    #DpOrganisationIdTable
+    FROM    rpd.SubmissionEvents AS SE
+            INNER JOIN rpd.cosmos_file_metadata AS CFM ON CFM.FileId = SE.FileId
+            INNER JOIN rpd.CompanyDetails AS CD ON CD.[FileName] = CFM.[FileName]
+            INNER JOIN (SELECT value AS FileId FROM STRING_SPLIT(@FileIds, @Delimiter)) F ON F.FileId = SE.FileId
+    WHERE   CD.subsidiary_id IS NULL
+    GROUP BY CD.organisation_id;
+
+    SELECT  CD.subsidiary_id AS OrganisationId
+    INTO    #SubOrganisationIdTable
+    FROM    rpd.CompanyDetails AS CD
+            INNER JOIN #DpOrganisationIdTable DID ON DID.OrganisationId = CD.organisation_id
+    WHERE   CD.subsidiary_id IS NOT NULL
+    GROUP BY CD.subsidiary_id;
 
     -- Variables to hold the two final CSV results
-    DECLARE @DpOrgList NVARCHAR(MAX);
-    DECLARE @SubOrgList NVARCHAR(MAX);
-    
-    SELECT @DpOrgList = STRING_AGG(CAST(OrganisationId AS NVARCHAR(MAX)), ',')
-    FROM #DpOrganisationIdTable;
-
-    SELECT @SubOrgList = STRING_AGG(CAST(OrganisationId AS NVARCHAR(MAX)), ',')
-    FROM #SubOrganisationIdTable;
+    DECLARE @DpOrgList NVARCHAR(MAX) = (SELECT STRING_AGG(CAST(OrganisationId AS NVARCHAR(MAX)), @Delimiter) FROM #DpOrganisationIdTable);
+    DECLARE @SubOrgList NVARCHAR(MAX) = (SELECT STRING_AGG(CAST(OrganisationId AS NVARCHAR(MAX)), @Delimiter) FROM #SubOrganisationIdTable);
 
     -- Call the child procedure
-    EXEC dbo.sp_ProcessOrganisationsToRetrieveObligationDecision
-        @DpOrgIds = @DpOrgList,
-        @SubOrgIds = @SubOrgList;
+    EXEC dbo.sp_ProcessOrganisationsToRetrieveObligationDecision @DpOrgIds = @DpOrgList, @SubOrgIds = @SubOrgList;
 END;
 GO
