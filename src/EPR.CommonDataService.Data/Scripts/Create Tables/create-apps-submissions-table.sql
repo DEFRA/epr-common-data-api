e74507b7a043466812a713273a734e65d387177d
Nicolai Negru <nicolai.negru@equalexperts.com>
2025-11-10 10:08:02 +0000

    add ProducerSize to apps.Submissions table

--- src/EPR.CommonDataService.Data/Scripts/Create Tables/create-apps-submissions-table.sql
+++ src/EPR.CommonDataService.Data/Scripts/Create Tables/create-apps-submissions-table.sql
@@ -18,12 +18,13 @@ BEGIN
         [UserId] [nvarchar](4000) NULL,
         [Type] [nvarchar](4000) NULL,
         [ComplianceSchemeId] [nvarchar](4000) NULL,
+        [ProducerSize] nchar(1) NULL,
         [load_ts] [datetime2](7) NULL
         )
         WITH
             (
             DISTRIBUTION = HASH ( [SubmissionId] ),
-        CLUSTERED COLUMNSTORE INDEX
+            CLUSTERED COLUMNSTORE INDEX
         )
 END;
 GO
