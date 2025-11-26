97dc960c9f26f51b2636e124083a4b08060c298e
Julian Young <julian.young.external@eviden.com>
2025-11-26 11:17:32 +0000

    Adjusted SubmissionSummary table creation: 1: Correct test for existence of the table 2: Convert the logic from CREATE and ALTER to DROP and CREATE for the new column NEW_FLAG

--- src/EPR.CommonDataService.Data/Scripts/Create Tables/create-submissions-summaries-table.sql
+++ src/EPR.CommonDataService.Data/Scripts/Create Tables/create-submissions-summaries-table.sql
@@ -1,47 +1,43 @@
-﻿IF NOT EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[apps].[RegistrationsSummaries]') AND type in (N'U'))
+﻿IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[apps].[SubmissionsSummaries]') AND type in (N'U'))
 BEGIN
-
-	CREATE TABLE [apps].[SubmissionsSummaries]
-	(
-		[SubmissionId] [nvarchar](4000) NULL,
-		[OrganisationId] [nvarchar](4000) NULL,
-		[ComplianceSchemeId] [nvarchar](4000) NULL,
-		[OrganisationName] [nvarchar](4000) NULL,
-		[OrganisationReference] [nvarchar](4000) NULL,
-		[OrganisationType] [nvarchar](4000) NULL,
-		[ProducerType] [nvarchar](4000) NULL,
-		[UserId] [nvarchar](4000) NULL,
-		[FirstName] [nvarchar](4000) NULL,
-		[LastName] [nvarchar](4000) NULL,
-		[Email] [nvarchar](4000) NULL,
-		[Telephone] [nvarchar](4000) NULL,
-		[ServiceRole] [nvarchar](4000) NULL,
-		[FileId] [nvarchar](4000) NULL,
-		[SubmissionYear] [int] NULL,
-		[SubmissionCode] [nvarchar](4000) NULL,
-		[ActualSubmissionPeriod] [nvarchar](4000) NULL,
-		[Combined_SubmissionCode] [nvarchar](4000) NULL,
-		[Combined_ActualSubmissionPeriod] [nvarchar](4000) NULL,
-		[SubmissionPeriod] [nvarchar](4000) NULL,
-		[SubmittedDate] [nvarchar](4000) NULL,
-		[Decision] [nvarchar](4000) NULL,
-		[IsResubmissionRequired] [bit] NULL,
-		[Comments] [nvarchar](4000) NULL,
-		[IsResubmission] [bit] NULL,
-		[PreviousRejectionComments] [nvarchar](4000) NULL,
-		[NationId] [int] NULL,
-		[PomFileName] NVARCHAR(4000),
-		[PomBlobName] NVARCHAR(4000)
-	)
-	WITH
-	(
-		DISTRIBUTION = HASH ( [SubmissionId] ),
-		CLUSTERED COLUMNSTORE INDEX
-	);
+	DROP TABLE [apps].[SubmissionsSummaries]
 END;
 
-IF NOT EXISTS (SELECT 1 FROM SYS.COLUMNS WHERE [Name] = N'NEW_FLAG' 
-	AND object_id = OBJECT_ID(N'[apps].[SubmissionsSummaries]'))
-BEGIN
-    ALTER TABLE [apps].[SubmissionsSummaries] ADD [NEW_FLAG] BIT NULL
-END;
+CREATE TABLE [apps].[SubmissionsSummaries]
+(
+	[SubmissionId] [nvarchar](4000) NULL,
+	[OrganisationId] [nvarchar](4000) NULL,
+	[ComplianceSchemeId] [nvarchar](4000) NULL,
+	[OrganisationName] [nvarchar](4000) NULL,
+	[OrganisationReference] [nvarchar](4000) NULL,
+	[OrganisationType] [nvarchar](4000) NULL,
+	[ProducerType] [nvarchar](4000) NULL,
+	[UserId] [nvarchar](4000) NULL,
+	[FirstName] [nvarchar](4000) NULL,
+	[LastName] [nvarchar](4000) NULL,
+	[Email] [nvarchar](4000) NULL,
+	[Telephone] [nvarchar](4000) NULL,
+	[ServiceRole] [nvarchar](4000) NULL,
+	[FileId] [nvarchar](4000) NULL,
+	[SubmissionYear] [int] NULL,
+	[SubmissionCode] [nvarchar](4000) NULL,
+	[ActualSubmissionPeriod] [nvarchar](4000) NULL,
+	[Combined_SubmissionCode] [nvarchar](4000) NULL,
+	[Combined_ActualSubmissionPeriod] [nvarchar](4000) NULL,
+	[SubmissionPeriod] [nvarchar](4000) NULL,
+	[SubmittedDate] [nvarchar](4000) NULL,
+	[Decision] [nvarchar](4000) NULL,
+	[IsResubmissionRequired] [bit] NULL,
+	[Comments] [nvarchar](4000) NULL,
+	[IsResubmission] [bit] NULL,
+	[PreviousRejectionComments] [nvarchar](4000) NULL,
+	[NationId] [int] NULL,
+	[PomFileName] NVARCHAR(4000),
+	[PomBlobName] NVARCHAR(4000),
+	[NEW_FLAG] BIT NULL
+)
+WITH
+(
+	DISTRIBUTION = HASH ( [SubmissionId] ),
+	CLUSTERED COLUMNSTORE INDEX
+);
