2890a0b198f214d141fc09505f7b4dd1be82bc44
Dan Sweeting <dansweeting@gmail.com>
2025-12-10 11:13:15 +0000

    feature: REEX-44: clone the `get-updated-producers` endpoint, expose similar with extra field (#316)
    * wip: clone the endpoint, expose similar with extra field
    
    * correction to route name
    
    * SubmissionYear => RegistrationYear
    
    * updates service to use latest agreed proc name
    
    * adds unit test to satisfy coverage metric
    
    * refactor: tidy up duplication to satify SONARQUBE
    
    * try suppressing with attribs
    
    suppress
    
    * Revert "refactor: tidy up duplication to satify SONARQUBE"
    
    This reverts commit 105b41a6732f0d32bcb1743892181697a74a88fd.
    
    * suppress other message
    
    * include V2 model for EF
    
    * adds tests to pass coverage bar
    
    * remove org id column
    
    * int -> string :|

--- src/EPR.CommonDataService.Api/Controllers/ProducerDetailsController.cs
+++ src/EPR.CommonDataService.Api/Controllers/ProducerDetailsController.cs
@@ -1,4 +1,5 @@
-﻿using EPR.CommonDataService.Api.Configuration;
+﻿using System.Diagnostics.CodeAnalysis;
+using EPR.CommonDataService.Api.Configuration;
 using EPR.CommonDataService.Core.Services;
 using EPR.CommonDataService.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -22,6 +23,23 @@ public class ProducerDetailsController(
     public async Task<IActionResult> GetUpdatedProducers(DateTime from, DateTime to)
     {
         var result = await producerDetailsService.GetUpdatedProducers(from, to);
+
+        if (result == null || result.Count == 0)
+        {
+            return NoContent();
+        }
+
+        return Ok(result);
+    }
+    
+    [HttpGet("updated-producers/", Name = nameof(GetUpdatedProducersV2))]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(List<UpdatedProducersResponseModelV2>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetUpdatedProducersV2(DateTime from, DateTime to)
+    {
+        var result = await producerDetailsService.GetUpdatedProducersV2(from, to);
 
         if (result == null || result.Count == 0)
         {
