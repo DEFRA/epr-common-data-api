fa8ab913f059569f29451c5649ff181ef5cd9489
Nicolai Negru <nicolai.negru@equalexperts.com>
2026-01-07 10:25:35 +0000

    append RegistrationJourney to the end of Submissions table

--- src/EPR.CommonDataService.Data/Scripts/Create Tables/create-apps-submissions-table.sql
+++ src/EPR.CommonDataService.Data/Scripts/Create Tables/create-apps-submissions-table.sql
@@ -18,8 +18,8 @@ BEGIN
         [UserId] [nvarchar](4000) NULL,
         [Type] [nvarchar](4000) NULL,
         [ComplianceSchemeId] [nvarchar](4000) NULL,
-        [RegistrationJourney] [nvarchar](128) NULL,
-        [load_ts] [datetime2](7) NULL
+        [load_ts] [datetime2](7) NULL,
+        [RegistrationJourney] [nvarchar](128) NULL
         )
         WITH
             (
