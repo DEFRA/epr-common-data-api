4fae11f0490dbe0c3cc2125275d04b6669e0138f
Nick Buckland <nicholas.buckland@equalexperts.com>
2026-01-09 08:57:25 +0000

    Added RegistrationJourney to OrganisationRegistrationSummaryDto

--- src/EPR.CommonDataService.Data/Entities/OrganisationRegistrationSummary.cs
+++ src/EPR.CommonDataService.Data/Entities/OrganisationRegistrationSummary.cs
@@ -1,35 +1,35 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
-namespace EPR.CommonDataService.Data.Entities
-{
-    /// <summary>
-    /// Used to return information the Organisation Registration Submissions List
-    /// </summary>
-    [ExcludeFromCodeCoverage]
-    public class OrganisationRegistrationSummaryDto
-    {
-        public Guid SubmissionId { get; set; }
-        public Guid OrganisationId { get; set; }
-        public string OrganisationType { get; set; }
-        public string OrganisationName { get; set; }
-        public string OrganisationReference { get; set; }
-        public string SubmissionStatus { get; set; }
-        public string StatusPendingDate { get; set; }
-        public string ApplicationReferenceNumber { get; set; }
-        public string RegistrationReferenceNumber { get; set; }
-        public int RelevantYear { get; set; }
-        public string SubmittedDateTime { get; set; }
-        public int NationId { get; set; }
-        public string RegulatorDecisionDate { get; set; }
-        public bool IsResubmission { get; set; }
-        public string ResubmissionStatus { get; set; }
-        public string ResubmissionDate { get; set; }
-        public string RegistrationDate { get; set; }
-    }
+namespace EPR.CommonDataService.Data.Entities;
 
-    [ExcludeFromCodeCoverage]
-    public class OrganisationRegistrationSummaryDataRow : OrganisationRegistrationSummaryDto
-    {
-        public int TotalItems { get; set; }
-    }
+/// <summary>
+/// Used to return information the Organisation Registration Submissions List
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class OrganisationRegistrationSummaryDto
+{
+    public Guid SubmissionId { get; set; }
+    public Guid OrganisationId { get; set; }
+    public string OrganisationType { get; set; }
+    public string RegistrationJourney { get; set; }
+    public string OrganisationName { get; set; }
+    public string OrganisationReference { get; set; }
+    public string SubmissionStatus { get; set; }
+    public string StatusPendingDate { get; set; }
+    public string ApplicationReferenceNumber { get; set; }
+    public string RegistrationReferenceNumber { get; set; }
+    public int RelevantYear { get; set; }
+    public string SubmittedDateTime { get; set; }
+    public int NationId { get; set; }
+    public string RegulatorDecisionDate { get; set; }
+    public bool IsResubmission { get; set; }
+    public string ResubmissionStatus { get; set; }
+    public string ResubmissionDate { get; set; }
+    public string RegistrationDate { get; set; }
+}
+
+[ExcludeFromCodeCoverage]
+public class OrganisationRegistrationSummaryDataRow : OrganisationRegistrationSummaryDto
+{
+    public int TotalItems { get; set; }
 }
