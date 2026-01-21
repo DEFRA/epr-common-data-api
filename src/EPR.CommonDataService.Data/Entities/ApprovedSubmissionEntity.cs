cbe10d74a5849d677b8a364cedea493a4c89c759
Nicholas Featch <nicholas.featch@equalexperts.com>
2026-01-21 20:59:53 +0000

    ECV-196 add NumberOfDaysObligated to ApprovedSubmissionEntity for partial obligation calculation

--- src/EPR.CommonDataService.Data/Entities/ApprovedSubmissionEntity.cs
+++ src/EPR.CommonDataService.Data/Entities/ApprovedSubmissionEntity.cs
@@ -6,8 +6,9 @@
         public string SubmissionPeriod { get; set; }
         public string PackagingMaterial { get; set; }
         public int PackagingMaterialWeight { get; set; }
-        public string OrganisationId { get; set; }
+        public Guid OrganisationId { get; set; }
         public Guid SubmitterId { get; set; }
         public string SubmitterType { get; set; }
+        public short? NumberOfDaysObligated { get; set; }
     }
 }
