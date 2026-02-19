4f6f30f19129cd0780aeac34cc53ef6e668156a2
Mike McBain <mike.mcbain@esynergy.co.uk>
2026-02-19 16:47:20 +0000

    feat(paycal): implement organization data streaming endpoint

---
+++ src/EPR.CommonDataService.Api/Features/PayCal/Organisations/StreamOut/OrganisationResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.CommonDataService.Api.Features.PayCal.Organisations.StreamOut;
+
+[ExcludeFromCodeCoverage]
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+public sealed record OrganisationResponse
+{
+    public int OrganisationId { get; init; }
+    public string? SubsidiaryId { get; init; }
+    public string? OrganisationName { get; init; }
+    public string? TradingName { get; init; }
+    public string? StatusCode { get; init; }
+    public string? ErrorCode { get; init; }
+    public string? JoinerDate { get; init; }
+    public string? LeaverDate { get; init; }
+    public string? ObligationStatus { get; init; }
+    public short? NumDaysObligated { get; init; }
+    public string? SubmitterId { get; init; }
+}
