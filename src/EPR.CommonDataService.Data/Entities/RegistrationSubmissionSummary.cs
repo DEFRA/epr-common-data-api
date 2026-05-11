9999fe7be5c300e5b938275ab6203532b293e816
dimitri work <dim@dimitri.daras.name>
2026-05-11 10:21:53 +0100

    SUB-40, DD. Stop sharing model between pom summaries and reg summaries due to different data types

--- src/EPR.CommonDataService.Data/Entities/RegistrationSubmissionSummary.cs
+++ src/EPR.CommonDataService.Data/Entities/RegistrationSubmissionSummary.cs
@@ -1,6 +1,6 @@
 namespace EPR.CommonDataService.Data.Entities;
 
-public class RegistrationSubmissionSummary : SubmissionSummaryModel
+public class RegistrationSubmissionSummary
 {
     public string CompaniesHouseNumber { get; set; }
     public string SubBuildingName { get; set; }
@@ -23,4 +23,25 @@ public class RegistrationSubmissionSummary : SubmissionSummaryModel
     public string BrandsFileName { get; set; }
     public string BrandsBlobName { get; set; }
     public string RegistrationDate { get; set; }
+    public Guid? SubmissionId { get; set; }
+    public Guid? OrganisationId { get; set; }
+    public Guid? ComplianceSchemeId { get; set; }
+    public string OrganisationName { get; set; }
+    
+    public string OrganisationReference { get; set; }
+    
+    public string OrganisationType { get; set; }
+    public string ProducerType { get; set; }
+    public Guid? UserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Telephone { get; set; }
+    public string ServiceRole { get; set; }
+    public string SubmissionPeriod { get; set; }
+    public string Decision { get; set; }
+    public string Comments { get; set; }
+    public bool IsResubmission { get; set; }
+    public string PreviousRejectionComments { get; set; }
+    public int NationId { get; set; }
 }
