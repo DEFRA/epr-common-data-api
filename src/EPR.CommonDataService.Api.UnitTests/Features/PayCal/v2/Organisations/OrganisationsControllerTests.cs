uk-gov-mirror/defra.epr-common-data-api

ECV-775: Adds PayCal v2 endpoints

src/EPR.CommonDataService.Api.UnitTests/Features/PayCal/v2/Organisations/OrganisationsControllerTests.cs
@@ -1,0 +1,191 @@
+using System.Diagnostics.CodeAnalysis;
+using EPR.CommonDataService.Api.Features.PayCal.v2.Organisations;
+using EPR.CommonDataService.Api.Features.PayCal.v2.Organisations.StreamOut;
+using EPR.CommonDataService.Api.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EPR.CommonDataService.Api.UnitTests.Features.PayCal.v2.Organisations;
+
+[ExcludeFromCodeCoverage]
+[TestClass]
+public class OrganisationsControllerTests
+{
+    private Mock<IStreamOrganisationsRequestHandler> _mockRequestHandler = null!;
+    private Mock<ILogger<OrganisationsController>> _mockLogger = null!;
+    private OrganisationsController _controller = null!;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockRequestHandler = new Mock<IStreamOrganisationsRequestHandler>();
+        _mockLogger = new Mock<ILogger<OrganisationsController>>();
+
+        _controller = new OrganisationsController(
+            _mockRequestHandler.Object,
+            _mockLogger.Object)
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            }
+        };
+    }
+
+    [TestMethod]
+    public void StreamOut_ShouldReturnNdJsonStreamResult()
+    {
+        // Arrange
+        var request = new StreamOrganisationsRequest { RelativeYear = 2025 };
+
+        var orgResponses = new List<OrganisationResponse>
+        {
+            new()
+            {
+                OrganisationId = 1,
+                SubsidiaryId = null,
+                OrganisationName = "Test Org",
+                TradingName = "Test Trading",
+                LeaverCode = "Active",
+                JoinerDate = "2024-01-01",
+                LeaverDate = null,
+                SubmitterId = "b2c3d4e5-f6a7-8901-bcde-f12345678901",
+                Filename = null,
+                SubmissionPeriodYear = 2026,
+                RegulatorStatus = "Accepted",
+                CreatedAt = null,
+                IsResubmission = false
+            }
+        };
+
+        _mockRequestHandler
+            .Setup(h => h.Handle(request, CancellationToken.None))
+            .Returns(orgResponses.ToAsyncEnumerable());
+
+        // Act
+        var result = _controller.StreamOut(request, CancellationToken.None);
+
+        // Assert
+        result.Should().BeOfType<NdJsonStreamResult<OrganisationResponse>>();
+    }
+
+    [TestMethod]
+    public async Task StreamOut_WhenStreamCompletesSuccessfully_ShouldLogCompletedSuccessfully()
+    {
+        // Arrange
+        var request = new StreamOrganisationsRequest { RelativeYear = 2025 };
+
+        static async IAsyncEnumerable<OrganisationResponse> GetRecords()
+        {
+            await Task.CompletedTask;
+            yield return new OrganisationResponse
+            {
+                OrganisationId = 1,
+                OrganisationName = "Test Org",
+                Filename = null,
+                SubsidiaryId = null,
+                SubmitterId = null,
+                TradingName = null,
+                LeaverCode = null,
+                LeaverDate = null,
+                JoinerDate = null,
+                SubmissionPeriodYear = 2026,
+                RegulatorStatus = "Accepted",
+                CreatedAt = null,
+                IsResubmission = false
+            };
+        }
+
+        _mockRequestHandler
+            .Setup(h => h.Handle(request, CancellationToken.None))
+            .Returns(GetRecords());
+
+        _controller.HttpContext.Response.Body = new MemoryStream();
+
+        // Act
+        var result = (NdJsonStreamResult<OrganisationResponse>)_controller.StreamOut(request, CancellationToken.None);
+        await result.ExecuteResultAsync(new ActionContext { HttpContext = _controller.HttpContext });
+
+        // Assert
+        _mockLogger.Verify(
+            logger => logger.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((state, _) => state.ToString()!.Contains("Completed successfully")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+    }
+
+    [TestMethod]
+    public async Task StreamOut_WhenStreamIsAbortedByClient_ShouldLogAbortedByClient()
+    {
+        // Arrange
+        var request = new StreamOrganisationsRequest { RelativeYear = 2025 };
+        var cts = new CancellationTokenSource();
+
+        async IAsyncEnumerable<OrganisationResponse> GetRecords()
+        {
+            yield return new OrganisationResponse
+            {
+                OrganisationId = 1,
+                OrganisationName = "Test Org",
+                Filename = null,
+                SubsidiaryId = null,
+                SubmitterId = null,
+                TradingName = null,
+                LeaverCode = null,
+                LeaverDate = null,
+                JoinerDate = null,
+                SubmissionPeriodYear = 2026,
+                RegulatorStatus = "Accepted",
+                CreatedAt = null,
+                IsResubmission = false
+            };
+            await cts.CancelAsync();
+            await Task.Delay(10, cts.Token); // throws OperationCanceledException
+        }
+
+        _mockRequestHandler
+            .Setup(h => h.Handle(request, CancellationToken.None))
+            .Returns(GetRecords());
+
+        _controller.HttpContext.Response.Body = new MemoryStream();
+        _controller.HttpContext.RequestAborted = cts.Token;
+
+        // Act
+        var result = (NdJsonStreamResult<OrganisationResponse>)_controller.StreamOut(request, CancellationToken.None);
+        await result.ExecuteResultAsync(new ActionContext { HttpContext = _controller.HttpContext });
+
+        // Assert
+        _mockLogger.Verify(
+            logger => logger.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((state, _) => state.ToString()!.Contains("Aborted by client")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+
+        cts.Dispose();
+    }
+
+    [TestMethod]
+    public void StreamOut_ShouldCallRequestHandlerWithRequestAndCancellationToken()
+    {
+        // Arrange
+        var request = new StreamOrganisationsRequest { RelativeYear = 2025 };
+        using var cts = new CancellationTokenSource();
+
+        _mockRequestHandler
+            .Setup(h => h.Handle(It.IsAny<StreamOrganisationsRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(new List<OrganisationResponse>().ToAsyncEnumerable());
+
+        // Act
+        _controller.StreamOut(request, cts.Token);
+
+        // Assert
+        _mockRequestHandler.Verify(h => h.Handle(request, cts.Token), Times.Once);
+    }
+}

src/EPR.CommonDataService.Api/Features/PayCal/v2/Organisations/StreamOut/OrganisationResponse.cs
@@ -1,0 +1,22 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.CommonDataService.Api.Features.PayCal.v2.Organisations.StreamOut;
+
+[ExcludeFromCodeCoverage]
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+public sealed record OrganisationResponse
+{
+    public required string? Filename { get; init; }
+    public required int OrganisationId { get; init; }
+    public required string? SubsidiaryId { get; init; }
+    public required string? SubmitterId { get; init; }
+    public required string? OrganisationName { get; init; }
+    public required string? TradingName { get; init; }
+    public required string? LeaverCode { get; init; }
+    public required string? LeaverDate { get; init; }
+    public required string? JoinerDate { get; init; }
+    public required int SubmissionPeriodYear { get; init; }
+    public required string RegulatorStatus { get; init; }
+    public required DateTimeOffset? CreatedAt { get; init; }
+    public required bool IsResubmission { get; init; }
+}

src/EPR.CommonDataService.Data/Infrastructure/SynapseContext.cs
@@ -27,7 +27,9 @@ public class SynapseContext : DbContext
     public DbSet<OrganisationRegistrationDetailsDto> OrganisationRegistrationSubmissionDetails { get; set; } = null!;
     public DbSet<RegistrationFeeCalculationDetailsModel> RegistrationFeeCalculationDetailsModel { get; set; } = null!;
     public DbSet<PayCalOrganisation> PayCalOrganisations { get; set; } = null!;
+    public DbSet<PayCalOrganisationV2> PayCalOrganisationsV2 { get; set; } = null!;
     public DbSet<PayCalPom> PayCalPoms { get; set; } = null!;
+    public DbSet<PayCalPomV2> PayCalPomsV2 { get; set; } = null!;
     public DbSet<OrganisationPom> OrganisationPoms { get; set; } = null!;
 
     private const string InMemoryProvider = "Microsoft.EntityFrameworkCore.InMemory";
@@ -51,7 +53,6 @@ public class SynapseContext : DbContext
 
         modelBuilder.Entity<PayCalOrganisation>(entity =>
         {
-            // The data source for this entity is a stored procedure - sp_GetPaycalOrgData
             entity.HasNoKey();
             entity.Property(e => e.OrganisationId).HasColumnName("organisation_id");
             entity.Property(e => e.SubsidiaryId).HasColumnName("subsidiary_id").HasMaxLength(4000);
@@ -69,9 +70,26 @@ public class SynapseContext : DbContext
             entity.Property(e => e.HasH2).HasColumnName("has_h2");
         });
 
+        modelBuilder.Entity<PayCalOrganisationV2>(entity =>
+        {
+            entity.HasNoKey();
+            entity.Property(e => e.Filename).HasColumnName("file_name");
+            entity.Property(e => e.OrganisationId).HasColumnName("organisation_id");
+            entity.Property(e => e.SubsidiaryId).HasColumnName("subsidiary_id").HasMaxLength(4000);
+            entity.Property(e => e.OrganisationName).HasColumnName("organisation_name").HasMaxLength(4000);
+            entity.Property(e => e.TradingName).HasColumnName("trading_name").HasMaxLength(4000);
+            entity.Property(e => e.JoinerDate).HasColumnName("joiner_date").HasMaxLength(4000);
+            entity.Property(e => e.LeaverCode).HasColumnName("status_code").HasMaxLength(4000);
+            entity.Property(e => e.LeaverDate).HasColumnName("leaver_date").HasMaxLength(4000);
+            entity.Property(e => e.SubmitterId).HasColumnName("submitter_id").HasMaxLength(4000);
+            entity.Property(e => e.SubmissionPeriodYear).HasColumnName("submission_period_year");
+            entity.Property(e => e.RegulatorStatus).HasColumnName("regulator_status").HasMaxLength(4000);
+            entity.Property(e => e.IsResubmission).HasColumnName("is_resubmission");
+            entity.Property(e => e.CreatedAt).HasColumnName("created_date_time").HasMaxLength(4000);
+        });
+
         modelBuilder.Entity<PayCalPom>(entity =>
         {
-            // The data source for this entity is a stored procedure - sp_GetPaycalPomData
             entity.HasNoKey();
             entity.Property(e => e.OrganisationId).HasColumnName("organisation_id");
             entity.Property(e => e.SubsidiaryId).HasColumnName("subsidiary_id").HasMaxLength(4000);
@@ -85,6 +103,26 @@ public class SynapseContext : DbContext
             entity.Property(e => e.PackagingMaterialSubtype).HasColumnName("packaging_material_subtype").HasMaxLength(4000);
             entity.Property(e => e.PackagingMaterialWeight).HasColumnName("packaging_material_weight");
             entity.Property(e => e.RamRagRating).HasColumnName("ram_rag_rating").HasMaxLength(4000);
+        });
+
+        modelBuilder.Entity<PayCalPomV2>(entity =>
+        {
+            entity.HasNoKey();
+            entity.Property(e => e.Filename).HasColumnName("file_name");
+            entity.Property(e => e.OrganisationId).HasColumnName("organisation_id");
+            entity.Property(e => e.SubsidiaryId).HasColumnName("subsidiary_id").HasMaxLength(4000);
+            entity.Property(e => e.SubmitterId).HasColumnName("submitter_id").HasMaxLength(4000);
+            entity.Property(e => e.SubmissionPeriod).HasColumnName("submission_period").HasMaxLength(4000);
+            entity.Property(e => e.SubmissionPeriodDescription).HasColumnName("submission_period_desc").HasMaxLength(4000);
+            entity.Property(e => e.PackagingActivity).HasColumnName("packaging_activity").HasMaxLength(4000);
+            entity.Property(e => e.PackagingType).HasColumnName("packaging_type").HasMaxLength(4000);
+            entity.Property(e => e.PackagingClass).HasColumnName("packaging_class").HasMaxLength(4000);
+            entity.Property(e => e.PackagingMaterial).HasColumnName("packaging_material").HasMaxLength(4000);
+            entity.Property(e => e.PackagingMaterialSubtype).HasColumnName("packaging_material_subtype").HasMaxLength(4000);
+            entity.Property(e => e.PackagingMaterialWeight).HasColumnName("packaging_material_weight");
+            entity.Property(e => e.RamRagRating).HasColumnName("ram_rag_rating").HasMaxLength(4000);
+            entity.Property(e => e.IsResubmission).HasColumnName("is_resubmission");
+            entity.Property(e => e.CreatedAt).HasColumnName("created_date_time").HasMaxLength(4000);
         });
 
         modelBuilder.Entity<OrganisationPom>(entity =>

DEFRA/epr-common-data-api

src/EPR.CommonDataService.Api.UnitTests/Features/PayCal/v2/Organisations/OrganisationsControllerTests.cs
@@ -1,0 +1,191 @@
+using System.Diagnostics.CodeAnalysis;
+using EPR.CommonDataService.Api.Features.PayCal.v2.Organisations;
+using EPR.CommonDataService.Api.Features.PayCal.v2.Organisations.StreamOut;
+using EPR.CommonDataService.Api.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EPR.CommonDataService.Api.UnitTests.Features.PayCal.v2.Organisations;
+
+[ExcludeFromCodeCoverage]
+[TestClass]
+public class OrganisationsControllerTests
+{
+    private Mock<IStreamOrganisationsRequestHandler> _mockRequestHandler = null!;
+    private Mock<ILogger<OrganisationsController>> _mockLogger = null!;
+    private OrganisationsController _controller = null!;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockRequestHandler = new Mock<IStreamOrganisationsRequestHandler>();
+        _mockLogger = new Mock<ILogger<OrganisationsController>>();
+
+        _controller = new OrganisationsController(
+            _mockRequestHandler.Object,
+            _mockLogger.Object)
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            }
+        };
+    }
+
+    [TestMethod]
+    public void StreamOut_ShouldReturnNdJsonStreamResult()
+    {
+        // Arrange
+        var request = new StreamOrganisationsRequest { RelativeYear = 2025 };
+
+        var orgResponses = new List<OrganisationResponse>
+        {
+            new()
+            {
+                OrganisationId = 1,
+                SubsidiaryId = null,
+                OrganisationName = "Test Org",
+                TradingName = "Test Trading",
+                StatusCode = "Active",
+                JoinerDate = "2024-01-01",
+                LeaverDate = null,
+                SubmitterId = "b2c3d4e5-f6a7-8901-bcde-f12345678901",
+                Filename = null,
+                SubmissionPeriodYear = 2026,
+                RegulatorStatus = "Accepted",
+                CreatedAt = null,
+                IsResubmission = false
+            }
+        };
+
+        _mockRequestHandler
+            .Setup(h => h.Handle(request, CancellationToken.None))
+            .Returns(orgResponses.ToAsyncEnumerable());
+
+        // Act
+        var result = _controller.StreamOut(request, CancellationToken.None);
+
+        // Assert
+        result.Should().BeOfType<NdJsonStreamResult<OrganisationResponse>>();
+    }
+
+    [TestMethod]
+    public async Task StreamOut_WhenStreamCompletesSuccessfully_ShouldLogCompletedSuccessfully()
+    {
+        // Arrange
+        var request = new StreamOrganisationsRequest { RelativeYear = 2025 };
+
+        static async IAsyncEnumerable<OrganisationResponse> GetRecords()
+        {
+            await Task.CompletedTask;
+            yield return new OrganisationResponse
+            {
+                OrganisationId = 1,
+                OrganisationName = "Test Org",
+                Filename = null,
+                SubsidiaryId = null,
+                SubmitterId = null,
+                TradingName = null,
+                StatusCode = null,
+                LeaverDate = null,
+                JoinerDate = null,
+                SubmissionPeriodYear = 2026,
+                RegulatorStatus = "Accepted",
+                CreatedAt = null,
+                IsResubmission = false
+            };
+        }
+
+        _mockRequestHandler
+            .Setup(h => h.Handle(request, CancellationToken.None))
+            .Returns(GetRecords());
+
+        _controller.HttpContext.Response.Body = new MemoryStream();
+
+        // Act
+        var result = (NdJsonStreamResult<OrganisationResponse>)_controller.StreamOut(request, CancellationToken.None);
+        await result.ExecuteResultAsync(new ActionContext { HttpContext = _controller.HttpContext });
+
+        // Assert
+        _mockLogger.Verify(
+            logger => logger.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((state, _) => state.ToString()!.Contains("Completed successfully")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+    }
+
+    [TestMethod]
+    public async Task StreamOut_WhenStreamIsAbortedByClient_ShouldLogAbortedByClient()
+    {
+        // Arrange
+        var request = new StreamOrganisationsRequest { RelativeYear = 2025 };
+        var cts = new CancellationTokenSource();
+
+        async IAsyncEnumerable<OrganisationResponse> GetRecords()
+        {
+            yield return new OrganisationResponse
+            {
+                OrganisationId = 1,
+                OrganisationName = "Test Org",
+                Filename = null,
+                SubsidiaryId = null,
+                SubmitterId = null,
+                TradingName = null,
+                StatusCode = null,
+                LeaverDate = null,
+                JoinerDate = null,
+                SubmissionPeriodYear = 2026,
+                RegulatorStatus = "Accepted",
+                CreatedAt = null,
+                IsResubmission = false
+            };
+            await cts.CancelAsync();
+            await Task.Delay(10, cts.Token); // throws OperationCanceledException
+        }
+
+        _mockRequestHandler
+            .Setup(h => h.Handle(request, CancellationToken.None))
+            .Returns(GetRecords());
+
+        _controller.HttpContext.Response.Body = new MemoryStream();
+        _controller.HttpContext.RequestAborted = cts.Token;
+
+        // Act
+        var result = (NdJsonStreamResult<OrganisationResponse>)_controller.StreamOut(request, CancellationToken.None);
+        await result.ExecuteResultAsync(new ActionContext { HttpContext = _controller.HttpContext });
+
+        // Assert
+        _mockLogger.Verify(
+            logger => logger.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((state, _) => state.ToString()!.Contains("Aborted by client")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+
+        cts.Dispose();
+    }
+
+    [TestMethod]
+    public void StreamOut_ShouldCallRequestHandlerWithRequestAndCancellationToken()
+    {
+        // Arrange
+        var request = new StreamOrganisationsRequest { RelativeYear = 2025 };
+        using var cts = new CancellationTokenSource();
+
+        _mockRequestHandler
+            .Setup(h => h.Handle(It.IsAny<StreamOrganisationsRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(new List<OrganisationResponse>().ToAsyncEnumerable());
+
+        // Act
+        _controller.StreamOut(request, cts.Token);
+
+        // Assert
+        _mockRequestHandler.Verify(h => h.Handle(request, cts.Token), Times.Once);
+    }
+}

src/EPR.CommonDataService.Api/Features/PayCal/v2/Organisations/StreamOut/OrganisationResponse.cs
@@ -1,0 +1,22 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.CommonDataService.Api.Features.PayCal.v2.Organisations.StreamOut;
+
+[ExcludeFromCodeCoverage]
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+public sealed record OrganisationResponse
+{
+    public required string? Filename { get; init; }
+    public required int OrganisationId { get; init; }
+    public required string? SubsidiaryId { get; init; }
+    public required string? SubmitterId { get; init; }
+    public required string? OrganisationName { get; init; }
+    public required string? TradingName { get; init; }
+    public required string? StatusCode { get; init; }
+    public required string? LeaverDate { get; init; }
+    public required string? JoinerDate { get; init; }
+    public required int SubmissionPeriodYear { get; init; }
+    public required string RegulatorStatus { get; init; }
+    public required DateTimeOffset? CreatedAt { get; init; }
+    public required bool IsResubmission { get; init; }
+}

src/EPR.CommonDataService.Data/Infrastructure/SynapseContext.cs
@@ -27,7 +27,9 @@ public class SynapseContext : DbContext
     public DbSet<OrganisationRegistrationDetailsDto> OrganisationRegistrationSubmissionDetails { get; set; } = null!;
     public DbSet<RegistrationFeeCalculationDetailsModel> RegistrationFeeCalculationDetailsModel { get; set; } = null!;
     public DbSet<PayCalOrganisation> PayCalOrganisations { get; set; } = null!;
+    public DbSet<PayCalOrganisationV2> PayCalOrganisationsV2 { get; set; } = null!;
     public DbSet<PayCalPom> PayCalPoms { get; set; } = null!;
+    public DbSet<PayCalPomV2> PayCalPomsV2 { get; set; } = null!;
     public DbSet<OrganisationPom> OrganisationPoms { get; set; } = null!;
 
     private const string InMemoryProvider = "Microsoft.EntityFrameworkCore.InMemory";
@@ -51,7 +53,6 @@ public class SynapseContext : DbContext
 
         modelBuilder.Entity<PayCalOrganisation>(entity =>
         {
-            // The data source for this entity is a stored procedure - sp_GetPaycalOrgData
             entity.HasNoKey();
             entity.Property(e => e.OrganisationId).HasColumnName("organisation_id");
             entity.Property(e => e.SubsidiaryId).HasColumnName("subsidiary_id").HasMaxLength(4000);
@@ -69,9 +70,26 @@ public class SynapseContext : DbContext
             entity.Property(e => e.HasH2).HasColumnName("has_h2");
         });
 
+        modelBuilder.Entity<PayCalOrganisationV2>(entity =>
+        {
+            entity.HasNoKey();
+            entity.Property(e => e.Filename).HasColumnName("file_name");
+            entity.Property(e => e.OrganisationId).HasColumnName("organisation_id");
+            entity.Property(e => e.SubsidiaryId).HasColumnName("subsidiary_id").HasMaxLength(4000);
+            entity.Property(e => e.OrganisationName).HasColumnName("organisation_name").HasMaxLength(4000);
+            entity.Property(e => e.TradingName).HasColumnName("trading_name").HasMaxLength(4000);
+            entity.Property(e => e.JoinerDate).HasColumnName("joiner_date").HasMaxLength(4000);
+            entity.Property(e => e.StatusCode).HasColumnName("status_code").HasMaxLength(4000);
+            entity.Property(e => e.LeaverDate).HasColumnName("leaver_date").HasMaxLength(4000);
+            entity.Property(e => e.SubmitterId).HasColumnName("submitter_id").HasMaxLength(4000);
+            entity.Property(e => e.SubmissionPeriodYear).HasColumnName("submission_period_year");
+            entity.Property(e => e.RegulatorStatus).HasColumnName("regulator_status").HasMaxLength(4000);
+            entity.Property(e => e.IsResubmission).HasColumnName("is_resubmission");
+            entity.Property(e => e.CreatedAt).HasColumnName("created_date_time").HasMaxLength(4000);
+        });
+
         modelBuilder.Entity<PayCalPom>(entity =>
         {
-            // The data source for this entity is a stored procedure - sp_GetPaycalPomData
             entity.HasNoKey();
             entity.Property(e => e.OrganisationId).HasColumnName("organisation_id");
             entity.Property(e => e.SubsidiaryId).HasColumnName("subsidiary_id").HasMaxLength(4000);
@@ -85,6 +103,26 @@ public class SynapseContext : DbContext
             entity.Property(e => e.PackagingMaterialSubtype).HasColumnName("packaging_material_subtype").HasMaxLength(4000);
             entity.Property(e => e.PackagingMaterialWeight).HasColumnName("packaging_material_weight");
             entity.Property(e => e.RamRagRating).HasColumnName("ram_rag_rating").HasMaxLength(4000);
+        });
+
+        modelBuilder.Entity<PayCalPomV2>(entity =>
+        {
+            entity.HasNoKey();
+            entity.Property(e => e.Filename).HasColumnName("file_name");
+            entity.Property(e => e.OrganisationId).HasColumnName("organisation_id");
+            entity.Property(e => e.SubsidiaryId).HasColumnName("subsidiary_id").HasMaxLength(4000);
+            entity.Property(e => e.SubmitterId).HasColumnName("submitter_id").HasMaxLength(4000);
+            entity.Property(e => e.SubmissionPeriod).HasColumnName("submission_period").HasMaxLength(4000);
+            entity.Property(e => e.SubmissionPeriodDescription).HasColumnName("submission_period_desc").HasMaxLength(4000);
+            entity.Property(e => e.PackagingActivity).HasColumnName("packaging_activity").HasMaxLength(4000);
+            entity.Property(e => e.PackagingType).HasColumnName("packaging_type").HasMaxLength(4000);
+            entity.Property(e => e.PackagingClass).HasColumnName("packaging_class").HasMaxLength(4000);
+            entity.Property(e => e.PackagingMaterial).HasColumnName("packaging_material").HasMaxLength(4000);
+            entity.Property(e => e.PackagingMaterialSubtype).HasColumnName("packaging_material_subtype").HasMaxLength(4000);
+            entity.Property(e => e.PackagingMaterialWeight).HasColumnName("packaging_material_weight");
+            entity.Property(e => e.RamRagRating).HasColumnName("ram_rag_rating").HasMaxLength(4000);
+            entity.Property(e => e.IsResubmission).HasColumnName("is_resubmission");
+            entity.Property(e => e.CreatedAt).HasColumnName("created_date_time").HasMaxLength(4000);
         });
 
         modelBuilder.Entity<OrganisationPom>(entity =>